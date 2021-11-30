abb9166bd62fbbde75a8e373cb55e3f8f2590218
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2021-11-30 08:26:39 +0200

    fix: use more realistic volume_per_ha values in example operations

--- sim/operations.py
+++ sim/operations.py
@@ -1,11 +1,11 @@
-from math import log
+from math import log, e
 from typing import Any, Optional
 
 
 def grow(volume: float) -> Optional[float]:
     if volume is None:
         return None
-    multiplier = 1 + 1 / log(volume, 10)
+    multiplier = 1 + 1 / log(volume, e)
     result = multiplier * volume
     print("Forest growing by factor of " + str(multiplier) + ". V = " + str(volume) + " -> " + str(multiplier * volume))
     return result
@@ -16,18 +16,19 @@ def cut(volume: float, pct: float = 100) -> Optional[float]:
         return None
     multiplier = pct / 100
     result = volume - multiplier * volume
-    minimum_remaining = 15000
-    if can_cut(result, minimum_remaining) is False:
-        raise Exception("Tree volume " + str(volume) + " is below cutting threshold " + str(minimum_remaining) + ".")
+    if can_cut(volume) is False:
+        raise Exception("Can not cut " + str(pct) + " %. Tree volume " + str(result) + " is below cutting threshold.")
+
     print("Cutting " + str(pct) + " %. V = " + str(volume) + " -> " + str(result))
     return result
 
 
-def can_cut(volume: float, threshold: float = 15000) -> bool:
+def can_cut(volume: float, threshold: float = 200) -> bool:
     return volume > threshold
 
 
 def do_nothing(data: Any) -> Any:
+    print("Did nothing")
     return data
 
 
