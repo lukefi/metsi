82e59c98833c9831c2f3a44f7ecb8ebb786b3213
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-02-01 10:07:08 +0200

    test: add test suite for investigating the yaml library

---
+++ tests/yaml_library_test.py
@@ -0,0 +1,40 @@
+import unittest
+import yaml
+
+class YamlLibraryTestSuite(unittest.TestCase):
+    simple_test_fixture = """
+    object_one:
+      property_one: 1
+      primitive_collection:
+        - item1
+        - item2
+      object_collection:
+        - param1: abc
+          param2: def
+        - param1: yes
+          param2: no 
+    """
+
+    anchors_test_fixture = """
+    variables:
+      var_one: &var_one 1
+    object_one:
+      property_one: *var_one
+    """
+
+    def test_simple_yaml_load(self):
+        result = yaml.load(self.simple_test_fixture)
+        self.assertEqual(1, result['object_one']['property_one'])
+        self.assertEqual(2, len(result['object_one']['primitive_collection']))
+        self.assertEqual('item1', result['object_one']['primitive_collection'][0])
+        self.assertEqual('item2', result['object_one']['primitive_collection'][1])
+        self.assertEqual('item2', result['object_one']['primitive_collection'][1])
+        self.assertEqual('abc', result['object_one']['object_collection'][0]['param1'])
+        self.assertEqual('def', result['object_one']['object_collection'][0]['param2'])
+        self.assertEqual(True, result['object_one']['object_collection'][1]['param1'])
+        self.assertEqual(False, result['object_one']['object_collection'][1]['param2'])
+
+    def test_anchors_yaml_load(self):
+        result = yaml.load(self.anchors_test_fixture)
+        self.assertEqual(1, result['variables']['var_one'])
+        self.assertEqual(1, result['object_one']['property_one'])
