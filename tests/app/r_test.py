44851d07b17d93cb8a2fe1e7ba854a29454b78a9
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-05-23 08:42:03 +0300

    test: add a case showing the usage of R data sctructures

--- tests/app/r_test.py
+++ tests/app/r_test.py
@@ -35,5 +35,31 @@ class RTest(unittest.TestCase):
         result = robjects.r['dbl'](data)
         self.assertEqual(20, result[0])
 
+    def test_python_to_r_transformations(self):
+        vector = [1, 2, 3]
+        matrix = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+        object = {
+            'one': 1,
+            'two': [1, 2, 3]
+        }
+
+        v = robjects.IntVector(vector)
+        m = robjects.r.matrix(robjects.IntVector(matrix), nrow=3)
+        df = robjects.DataFrame(object)
+
+        self.assertEqual(vector, list(v))
+        self.assertEqual(matrix, list(m))
+        self.assertEqual(1, m[0])
+        self.assertEqual(['one', 'two.1L', 'two.2L', 'two.3L'], list(df.colnames))
+
+        one = df.rx('one')[0]
+        two1 = df.rx('two.1L')[0]
+        two2 = df.rx('two.2L')[0]
+        two3 = df.rx('two.3L')[0]
+        self.assertEqual([1], list(one))
+        self.assertEqual([1], list(two1))
+        self.assertEqual([2], list(two2))
+        self.assertEqual([3], list(two3))
+
 
 
