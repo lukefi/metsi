17bcb7eb5f246aef3f22280e3bdf0bbe1e87cbb9
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-06-03 10:50:21 +0300

    test: update argparse test case

--- tests/app/app_io_test.py
+++ tests/app/app_io_test.py
@@ -4,10 +4,12 @@ from app import app_io as aio
 
 class TestAppIO(unittest.TestCase):
     def test_app_io(self):
-        args = ['input.json', 'control.yaml']
+        args = ['input.json', 'control.yaml', 'output.pickle', '-s', 'partial']
         result = aio.parse_cli_arguments(args)
         print(result)
         print(dir(result))
-        self.assertEqual(2, len(result.__dict__.keys()))
+        self.assertEqual(4, len(result.__dict__.keys()))
         self.assertEqual('input.json', result.domain_state_file)
         self.assertEqual('control.yaml', result.control_file)
+        self.assertEqual('output.pickle', result.output_file)
+        self.assertEqual('partial', result.strategy)
