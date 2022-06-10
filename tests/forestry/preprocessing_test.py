e6cdab5ef20b39f078a9bcd494bc0aeb91e928e1
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-06-10 16:38:06 +0300

    test: make preprocessing tests more readable

--- tests/forestry/preprocessing_test.py
+++ tests/forestry/preprocessing_test.py
@@ -4,19 +4,19 @@ import forestry.preprocessing as preprocessing
 from forestdatamodel.model import ForestStand, ReferenceTree
 from forestdatamodel.enums.internal import TreeSpecies
 
-def generate_sapling_tree_stands(sapling_tree_count, reference_tree_count):
+def generate_stand_with_saplings(sapling_tree_count, reference_tree_count):
     """generates a ForestStand with a given number of ReferenceTrees of which a given number is sapling trees"""
-    is_sapling_list = [True if i<=sapling_tree_count else False for i in range(1, reference_tree_count+1)]
     stand = ForestStand()
-    stand.reference_trees = [ReferenceTree(sapling=s) for s in is_sapling_list]
+    for i in range(reference_tree_count):
+        is_sapling = i < sapling_tree_count
+        stand.reference_trees.append(ReferenceTree(sapling=is_sapling))
     return stand
-
+    
 def generate_empty_stands(stand_count, empty_stand_count):
-    is_empty_stand_list = [True if i<=empty_stand_count else False for i in range(1, stand_count+1)]
     stands = []
     for i in range(0, stand_count):
         stand = ForestStand()  
-        stand_is_empty = is_empty_stand_list[i] 
+        stand_is_empty = i < empty_stand_count
         #if the stand is not meant to be empty, add one Reference tree. 
         if not stand_is_empty:
             stand.reference_trees.append(ReferenceTree(species=TreeSpecies(1)))
@@ -30,8 +30,8 @@ class PreprocessingTest(unittest.TestCase):
         sapling_tree_count = 2
         reference_tree_count = 4
         #create a list of one stand
-        stands = [generate_sapling_tree_stands(sapling_tree_count, reference_tree_count)]
-        excluded = preprocessing.exclude_sapling_trees(stands) #do not pass kwargs
+        stands = [generate_stand_with_saplings(sapling_tree_count, reference_tree_count)]
+        excluded = preprocessing.exclude_sapling_trees(stands)
         #count of reference trees in the stand after sapling exclusion should equal to the original count minus saplings
         self.assertEqual(len(excluded[0].reference_trees), reference_tree_count - sapling_tree_count)
 
@@ -40,5 +40,5 @@ class PreprocessingTest(unittest.TestCase):
         empty_stand_count = 2
 
         stands = generate_empty_stands(stand_count, empty_stand_count)
-        excluded = preprocessing.exclude_empty_stands(stands) #do not pass kwargs
+        excluded = preprocessing.exclude_empty_stands(stands)
         self.assertEqual(len(excluded), stand_count - empty_stand_count)
