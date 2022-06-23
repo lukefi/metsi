dd680c892ad119bc23dbf064a0513aed066ae63a
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-06-23 15:23:55 +0300

    test: add test to make sure cross_cut_stand returns sensible numbers

--- tests/forestry/cross_cutting_test.py
+++ tests/forestry/cross_cutting_test.py
@@ -1,7 +1,8 @@
 import unittest
 from forestdatamodel.model import ForestStand, ReferenceTree
+from forestdatamodel.enums.internal import TreeSpecies
 import rpy2.robjects as robjects
-from forestry import r_utils
+from forestry import r_utils, cross_cutting
 class CrossCuttingTest(unittest.TestCase):
 
     def test_cross_cut_tree(self):
@@ -18,4 +19,25 @@ class CrossCuttingTest(unittest.TestCase):
         # cross cutting is done individually to each referenceTree (use dbh, height and species)
         # pass in Puutavaralajimäärittelyt.txt? <-- this probably needs to be user-inputted?
 
-        
+    def test_cross_cut_stand_returns_total_values(self):
+        """This test ensures that the cross_cut_stand returns values that are multiplied by the reference tree's stem count per ha and the stand area."""
+       
+       #parameter values selected arbitrarily 
+        tree = ReferenceTree(
+            species=TreeSpecies.PINE,
+            breast_height_diameter=45.678,
+            height=28.43,
+            stems_per_ha=22.3
+        )
+
+        stand = ForestStand(
+            reference_trees=[tree],
+            area=296.23
+        )
+
+        volumes, values = cross_cutting.cross_cut_stand(stand)
+
+        self.assertEqual(volumes[0], [12.459322454417418, 0.26732088300923035])
+        self.assertEqual(values[0], [735.1000248106276, 4.544455011156915])
+
+
