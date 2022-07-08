d781e8982c49db9d26e5b3683ceb590bf2f15ed9
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-07-08 12:38:05 +0300

    test: add tests cases for parameters propagation to operations

--- tests/sim/runners_test.py
+++ tests/sim/runners_test.py
@@ -5,8 +5,6 @@ from tests.test_utils import raises, identity, none, collect_results, load_yaml,
 
 
 class TestOperations(unittest.TestCase):
-    declaration = load_yaml('branching.yaml')
-
     def test_sequence_success(self):
         payload = OperationPayload(simulation_state=1)
         result = evaluate_sequence(
@@ -27,15 +25,42 @@ class TestOperations(unittest.TestCase):
         self.assertRaises(Exception, prepared_function)
 
     def test_strategies_by_comparison(self):
+        declaration = load_yaml('branching.yaml')
         initial = OperationPayload(
             simulation_state=1,
             run_history={},
-            aggregated_results={ 'operation_results': {} }
+            aggregated_results={'operation_results': {}}
         )
         results_full = collect_results(
-            run_full_tree_strategy(initial, self.declaration, {'inc': aggregating_increment}))
+            run_full_tree_strategy(initial, declaration, {'inc': aggregating_increment}))
         results_partial = collect_results(
-            run_partial_tree_strategy(initial, self.declaration, {'inc': aggregating_increment}))
+            run_partial_tree_strategy(initial, declaration, {'inc': aggregating_increment}))
         self.assertEqual(8, len(results_full))
         self.assertEqual(8, len(results_partial))
         self.assertEqual(results_partial, results_full)
+
+    def test_no_parameters_propagation(self):
+        declaration = load_yaml('no_parameters.yaml')
+        initial = OperationPayload(
+            simulation_state=1,
+            run_history={},
+            aggregated_results={'operation_results': {}}
+        )
+
+        results = collect_results(
+            run_full_tree_strategy(initial, declaration, {'inc': aggregating_increment})
+        )
+        self.assertEqual(5, results[0])
+
+    def test_parameters_propagation(self):
+        declaration = load_yaml('parameters.yaml')
+        initial = OperationPayload(
+            simulation_state=1,
+            run_history={},
+            aggregated_results={'operation_results': {}}
+        )
+
+        results = collect_results(
+            run_full_tree_strategy(initial, declaration, {'inc': aggregating_increment})
+        )
+        self.assertEqual(9, results[0])
