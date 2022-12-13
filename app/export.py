d49444636cc87bc3a32f4ad80ea2c03dab51390c
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-13 13:50:37 +0200

    feat: first implementation of per stand per schedule, yearly stock-timber-sum output
    Missing explicit division between State and Node years
    Missing access to thinning operation type
    
    Including a location scrambled and pseudonymized SMK data example

--- app/export.py
+++ app/export.py
@@ -1,15 +1,21 @@
+from pathlib import Path
+
 from app.app_io import Mela2Configuration
+from app.app_types import SimResults
 from app.export_handlers.j import j_out, parse_j_config
 from app.console_logging import print_logline
+from app.export_handlers.rm_timber import rm_schedules_events_timber
 
 
-def export_files(config: Mela2Configuration, decl, data):
+def export_files(config: Mela2Configuration, decl: list[dict], data: SimResults):
     output_handlers = []
     for export_module_declaration in decl:
         format = export_module_declaration.get("format", None)
         if format == "J":
             j_config = parse_j_config(config, export_module_declaration)
             output_handlers.append(lambda: j_out(data, **j_config))
+        elif format == "rm_schedules_events_timber":
+            output_handlers.append(lambda: rm_schedules_events_timber(Path(config.target_directory, export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")), data))
         else:
             print_logline("Unknown output format for export: '{}'".format(format))
     for handler in output_handlers:
