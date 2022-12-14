f871f260fe3ddd77e8fb8b2ce5c3c50f54c184f7
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-14 15:53:34 +0200

    feat: console info logging for export runs

--- app/export.py
+++ app/export.py
@@ -10,13 +10,18 @@ from app.export_handlers.rm_timber import rm_schedules_events_timber
 def export_files(config: Mela2Configuration, decl: list[dict], data: SimResults):
     output_handlers = []
     for export_module_declaration in decl:
-        format = export_module_declaration.get("format", None)
-        if format == "J":
+        export_module = export_module_declaration.get("format", None)
+        if export_module == "J":
             j_config = parse_j_config(config, export_module_declaration)
-            output_handlers.append(lambda: j_out(data, **j_config))
-        elif format == "rm_schedules_events_timber":
-            output_handlers.append(lambda: rm_schedules_events_timber(Path(config.target_directory, export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")), data))
+            output_handlers.append((export_module,
+                                    lambda: j_out(data, **j_config)))
+        elif export_module == "rm_schedules_events_timber":
+            filename = export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")
+            target_path = Path(config.target_directory, filename)
+            output_handlers.append((export_module,
+                                    lambda: rm_schedules_events_timber(target_path, data)))
         else:
-            print_logline("Unknown output format for export: '{}'".format(format))
-    for handler in output_handlers:
+            print_logline("Unknown output format for export: '{}'".format(export_module))
+    for export_module, handler in output_handlers:
+        print_logline(f"Exporting {export_module}...")
         handler()
