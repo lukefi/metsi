2553c0bf52526d4dec2f3b57c8cda93c5c276c32
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-20 17:40:32 +0200

    test: make main test useful again
    staying to the point of simply verifying the main run

--- tests/app/main_test.py
+++ tests/app/main_test.py
@@ -8,97 +8,26 @@ import os
 import sys
 import shutil
 import unittest
-from pathlib import Path
 from app import mela2
 
 
-def run_simulator(state_input_files: list[str], state_output_containers: list[str]):
-
-    strategies = ['full', 'partial']
-    control_file = 'tests/resources/main_test/main_test_control.yaml'
-    run_details = []
-    output_details = []
-
-    for i, (container, file) in enumerate(zip(state_output_containers, state_input_files)):
-        for strategy in strategies:
-            run_details.append((
-                os.path.join(os.getcwd(), file),
-                container,
-                f"outdir{i}-{strategy}",
-                f"output.{container}",
-                strategy
-            ))
-            output_details.append(f"outdir{i}-{strategy}")
-
-    for input_file, state_output_container, output_dir, _, strategy in run_details:
+class MainTest(unittest.TestCase):
+    def test_mela2(self):
         sys.argv = [
-            'foo',
-            '-s',
-            strategy,
+            'mela2',
             '--state-format',
-            'fdm',
-            '--state-input-container',
-            state_output_container,
+            'forest_centre',
             '--preprocessing-output-container',
-            'pickle',
-            '--state-output-container',
-            state_output_container,
+            'csv',
             '-r',
-            'preprocess,simulate',
-            input_file,
-            output_dir,
-            control_file
+            'preprocess,simulate,postprocess,export',
+            'data/SMK_source.xml',
+            'test_outdir'
         ]
         mela2.main()
-    return output_details
-
-
-class MainTest(unittest.TestCase):
-
-    input_files = [
-        'tests/resources/main_test/two_ffc_stands.pickle',
-        'tests/resources/main_test/two_vmi12_stands_as_jsonpickle.json'
-    ]
-    input_containers = [
-        'pickle',
-        'json'
-    ]
-
-    def verify_result_dir(self, outdir):
-        _, stands, _ = next(os.walk(outdir))
-        self.assertEqual(2, len(stands))
-        for stand in stands:
-            _, schedules, _ = next(os.walk(Path(outdir, stand)))
-            for schedule in schedules:
-                _, _, files = next(os.walk(Path(outdir, stand, schedule)))
-                self.assertEqual(2, len(files))
-
-    def test_sim_main(self):
-        sim_results = run_simulator(self.input_files, self.input_containers)
-        for output_dir in sim_results:
-            self.verify_result_dir(output_dir)
-            preprocessing_filepath = Path(output_dir, 'preprocessing_result.pickle')
-            self.assertTrue(os.path.exists(preprocessing_filepath))
-            shutil.rmtree(output_dir)
-
-    def test_post_processing_main(self):
-        sim_results = run_simulator(self.input_files, self.input_containers)
-        for i, sim_dir in enumerate(sim_results):
-            pp_input_dir = sim_dir
-            pp_control_file = 'tests/resources/main_test/pp_test_control.yaml'
-            pp_output_dir = 'pp_outdir'
-
-            sys.argv = [
-                'foo',
-                pp_input_dir,
-                pp_output_dir,
-                pp_control_file,
-                '-r',
-                'postprocess'
-            ]
-
-            mela2.main()
-
-            self.verify_result_dir(pp_output_dir)
-            shutil.rmtree(pp_output_dir)
-            shutil.rmtree(sim_dir)
+        self.assertTrue(os.path.exists('test_outdir/preprocessing_result.csv'))
+        self.assertTrue(os.path.exists('test_outdir/data.cda'))
+        self.assertTrue(os.path.exists('test_outdir/data.xda'))
+        self.assertTrue(os.path.exists('test_outdir/trees.txt'))
+        self.assertTrue(os.path.exists('test_outdir/timber_sums.txt'))
+        shutil.rmtree('test_outdir')
