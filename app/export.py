5c5fd085cb85ed463e0d970360dac78ad317662f
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-20 09:37:13 +0200

    fix: avoid python namespace/object-reference clash

--- app/export.py
+++ app/export.py
@@ -16,17 +16,17 @@ def export_files(config: Mela2Configuration, decl: list[dict], data: SimResults)
             output_handlers.append((export_module,
                                     lambda: j_out(data, **j_config)))
         elif export_module == "rm_schedules_events_timber":
-            filename = export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")
-            target_path = Path(config.target_directory, filename)
+            filename1 = export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")
+            target_path1 = Path(config.target_directory, filename1)
             output_handlers.append((export_module,
-                                    lambda: rm_schedules_events_timber(target_path, data)))
+                                    lambda: rm_schedules_events_timber(target_path1, data)))
         elif export_module == "rm_schedules_events_trees":
-            filename = export_module_declaration.get("filename", "rm_schedule_trees.txt")
-            target_path = Path(config.target_directory, filename)
+            filename2 = export_module_declaration.get("filename", "rm_schedule_trees.txt")
+            target_path2 = Path(config.target_directory, filename2)
             output_handlers.append((export_module,
-                                    lambda: rm_schedules_events_trees(target_path, data)))
+                                    lambda: rm_schedules_events_trees(target_path2, data)))
         else:
-            print_logline("Unknown output format for export: '{}'".format(export_module))
+            print_logline(f"Unknown output format for export: '{export_module}'")
     for export_module, handler in output_handlers:
         print_logline(f"Exporting {export_module}...")
         handler()
