af9694052f608c1c31bb0b3bc0e09d90882f79a5
dmela <daniel.melander@luke.fi>
2025-04-02 12:17:56 +0300

    ref: new run modes logic
    Configuration as CLI and control parameters. Validation and sorting of the run modes ensure the pipeline order. Exceptions for incorrect values. Includes tests.

--- lukefi/metsi/app/app_io.py
+++ lukefi/metsi/app/app_io.py
@@ -1,22 +1,40 @@
 import argparse
-from enum import Enum
+from enum import IntEnum
 from types import SimpleNamespace
-from typing import Optional
+from lukefi.metsi.app.utils import ConfigurationException
 
 
-class RunMode(Enum):
-    PREPROCESS = "preprocess"
-    SIMULATE = "simulate"
-    POSTPROCESS = "postprocess"
-    EXPORT = "export"
-    EXPORT_PREPRO = "export_prepro"
+class RunMode(IntEnum):
+    PREPROCESS = 1
+    EXPORT_PREPRO = 2
+    SIMULATE = 3
+    POSTPROCESS = 4
+    EXPORT = 5
+
+    @classmethod
+    def from_str(cls, name: str):
+        try:
+            return cls[name.upper()]
+        except KeyError:
+            raise ValueError(f"Invalid run mode: {name}")
+
+    def __str__(self):
+        return self.name
+    
+    def __eq__(self, value):
+        if isinstance(value, str):
+            return self.name.lower() == value.lower()
+        return super().__eq__(value)
+    
+    def __hash__(self):
+        return hash(self.name)
 
 
 class MetsiConfiguration(SimpleNamespace):
     input_path = None
     target_directory = None
     control_file = "control.py"
-    run_modes = [RunMode.PREPROCESS, RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT]
+    run_modes = [RunMode.PREPROCESS, RunMode.EXPORT_PREPRO, RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT]
     state_format = "fdm"
     state_input_container = "csv"
     state_output_container = None
@@ -28,34 +46,41 @@ class MetsiConfiguration(SimpleNamespace):
     strata_origin = "1"
 
     def __init__(self, **kwargs):
-        if kwargs.get('run_modes') is not None:
-            self.set_run_modes(kwargs['run_modes'])
+        run_modes = kwargs.get('run_modes')
+        if run_modes is not None:
+            run_modes = run_modes.split(',') if isinstance(run_modes, str) else run_modes
+            self.run_modes = self._validate_and_sort_run_modes(run_modes)
             kwargs.__delitem__('run_modes')
         super().__init__(**kwargs)
 
-    def set_run_modes(self, run_modes: str):
+    @staticmethod
+    def _validate_and_sort_run_modes(modes: list[str]) -> list[RunMode]:
         try:
-            if isinstance(run_modes, list):
-                self.run_modes = [RunMode(mode) for mode in run_modes]
-            else:
-                self.run_modes = [RunMode(mode) for mode in run_modes.split(",")]
-        except ValueError:
-            allowed_modes = ", ".join(mode.value for mode in RunMode)
-            raise ValueError(f"Invalid run modes '{run_modes}'. Allowed modes: {allowed_modes}")
+            # given input to run mode instances 
+            mode_enums = [ RunMode.from_str(p) for p in modes ]
+        except ValueError as e:
+            raise ConfigurationException(e)  # Return error if invalid run mode
+
+        sorted_modes = sorted(mode_enums)
+
+        # Verify order conditions
+        if RunMode.EXPORT_PREPRO in sorted_modes and RunMode.PREPROCESS not in sorted_modes:
+            raise ConfigurationException(f"Error: Run mode { RunMode.EXPORT_PREPRO } can not be without { RunMode.POSTPROCESS }")
+
+        if RunMode.EXPORT in sorted_modes and not any(p in sorted_modes for p in [RunMode.SIMULATE, RunMode.POSTPROCESS]):
+            raise ConfigurationException(f"Error: Run mode { RunMode.EXPORT } is possible when { RunMode.SIMULATE } or { RunMode.POSTPROCESS } is included")
+
+        return [p for p in sorted_modes]
 
 
-def remove_nones(source: dict) -> dict:
-    """Return a dict with items from 'source' with keys mapping to None removed"""
-    filtered = {}
-    for k, v in source.items():
-        if v is not None:
-            filtered[k] = v
-    return filtered
+def _remove_nones(data: dict) -> dict:
+    """Internal function to remove None values from a dict."""
+    return { k:v for k,v in data.items() if v is not None }
 
 
 def generate_program_configuration(cli_args: argparse.Namespace, control_source: dict={}) -> MetsiConfiguration:
-    """Generate a Mela2Configuration, overriding values with control file source, then with CLI arguments"""
-    cli_source = remove_nones(cli_args.__dict__)
+    """ Factory for generating a MetsiConfiguration. CLI values are overriden with control file source."""
+    cli_source = _remove_nones(cli_args.__dict__)
 
     # DEPRECATED: Backwards compatiblity until next version
     if control_source.get('strategy') is not None:
@@ -70,27 +95,13 @@ def generate_program_configuration(cli_args: argparse.Namespace, control_source:
     return result
 
 
-def set_default_arguments(cli_args: argparse.Namespace, default_args: dict) -> argparse.Namespace:
-    """If args Namespace has its given member as None, in-place replace it with value from defaults"""
-    for member in [
-        'state_format',
-        'state_input_container',
-        'state_output_container',
-        'preprocessing_output_container',
-        'derived_data_output_container'
-    ]:
-        if cli_args.__dict__.get(member) is None:
-            cli_args.__dict__[member] = default_args.get(member)
-    return cli_args
-
-
 def parse_cli_arguments(args: list[str]):
     parser = argparse.ArgumentParser(description='Mela2.0 forest growth calculator. CLI arguments override matching control file arguments.')
     parser.add_argument('input_path', help='Application input file or directory')
     parser.add_argument('target_directory', help='Directory path for program output')
     parser.add_argument('control_file', nargs='?', help='Application control declaration file')
     parser.add_argument('-r', '--run-modes',
-                        help='Comma separated list of run modes. Possible modes: preprocess,simulate,postprocess,export',
+                        help='Comma separated list of run modes. Possible modes: preprocess,export_prepro,simulate,postprocess,export',
                         type=str)
     parser.add_argument('-s', '--strategy',
                         type=str,
@@ -113,10 +124,6 @@ def parse_cli_arguments(args: list[str]):
                         choices=['pickle', 'json', 'csv'],
                         type=str,
                         help='Container format of state output files: \'pickle\' (default), \'json\', \'csv\'')
-    parser.add_argument('--preprocessing-output-container',
-                        choices=['pickle', 'json', 'csv'],
-                        type=str,
-                        help='Container format of preprocessing result file: \'pickle\' (default), \'json\', \'csv\'')
     parser.add_argument('--derived-data-output-container',
                         choices=['pickle', 'json'],
                         type=str,
@@ -135,3 +142,10 @@ def parse_cli_arguments(args: list[str]):
                         action=argparse.BooleanOptionalAction,
                         help="Enable parallel simulation of computational units over the amount of CPU cores available. DEPRECATED: ignored")
     return parser.parse_args(args)
+
+__all__ = [
+    'MetsiConfiguration',
+    'RunMode',
+    'generate_program_configuration',
+    'parse_cli_arguments',
+]
--- tests/app/app_io_test.py
+++ tests/app/app_io_test.py
@@ -1,27 +1,17 @@
 import unittest
-
-import lukefi.metsi.app.app_io
-from lukefi.metsi.app import app_io as aio
-from lukefi.metsi.app.app_io import set_default_arguments, RunMode
+import pytest
+from lukefi.metsi.app.utils import ConfigurationException
+from lukefi.metsi.app.app_io import MetsiConfiguration, RunMode, \
+    parse_cli_arguments, generate_program_configuration
 
 
 class TestAppIO(unittest.TestCase):
-    def test_set_default_io_arguments(self):
-        default_io_arguments = {
-            'state_format': 'vmi12',
-            'state_output_container': 'pickle'
-        }
-        args = ['input.pickle', 'control.yaml', 'output2', '-s', 'partial', '--state-format', 'fdm', '--measured-trees', '--strata-origin', '2']
-        result = set_default_arguments(aio.parse_cli_arguments(args), default_io_arguments)
-        self.assertEqual(None, result.preprocessing_output_container)
-        self.assertEqual(None, result.state_input_container)
-        self.assertEqual('pickle', result.state_output_container)
-        self.assertEqual('fdm', result.state_format)
 
     def test_sim_cli_arguments(self):
-        args = ['input.pickle', 'output2', 'control.yaml', '-f', 'partial', '-e', 'depth', '--state-format', 'fdm', '--measured-trees', '--strata-origin', '2']
-        result = aio.parse_cli_arguments(args)
-        self.assertEqual(16, len(result.__dict__.keys()))
+        args = ['input.pickle', 'output2', 'control.yaml', '-f', 'partial', '-e', 'depth', '--state-format',
+                'fdm', '--measured-trees', '--strata-origin', '2', '-r', 'preprocess,simulate,export']
+        result = parse_cli_arguments(args)
+        self.assertEqual(15, len(result.__dict__.keys()))
         self.assertEqual('input.pickle', result.input_path)
         self.assertEqual('control.yaml', result.control_file)
         self.assertEqual('output2', result.target_directory)
@@ -32,47 +22,13 @@ class TestAppIO(unittest.TestCase):
         self.assertEqual(None, result.state_output_container)
         self.assertEqual(True, result.measured_trees)
         self.assertEqual("2", result.strata_origin)
+        self.assertEqual("preprocess,simulate,export", result.run_modes)
 
-    @unittest.skip
-    def test_run_modes(self):
-        successes = [
-            ("preprocess,simulate,postprocess,export", [RunMode.PREPROCESS, RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT]),
-            ("preprocess,simulate,export", [RunMode.PREPROCESS, RunMode.SIMULATE, RunMode.EXPORT]),
-            ("preprocess,simulate,postprocess", [RunMode.PREPROCESS, RunMode.SIMULATE, RunMode.POSTPROCESS]),
-            ("preprocess,simulate", [RunMode.PREPROCESS, RunMode.SIMULATE]),
-            ("preprocess", [RunMode.PREPROCESS]),
-            ("simulate,postprocess,export", [RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT]),
-            ("simulate,postprocess", [RunMode.SIMULATE, RunMode.POSTPROCESS]),
-            ("simulate,export", [RunMode.SIMULATE, RunMode.EXPORT]),
-            ("simulate", [RunMode.SIMULATE]),
-            ("postprocess,export", [RunMode.POSTPROCESS, RunMode.EXPORT]),
-            ("postprocess", [RunMode.POSTPROCESS]),
-            ("export", [RunMode.EXPORT])
-        ]
-        failures = [
-            "preprocess,postprocess,export",
-            "preprocess,postprocess",
-            "preprocess,export",
-            "simulate,preprocess",
-            "postprocess,preprocess",
-            "postprocess,simulate",
-            "export,preprocess",
-            "abc",
-            123,
-            "preporcess,simulate"
-        ]
-
-        for case in successes:
-            result = lukefi.metsi.app.app_io.MetsiConfiguration(run_modes=case[0]).run_modes
-            self.assertEqual(case[1], result)
-        for case in failures:
-            self.assertRaises(Exception, lukefi.metsi.app.app_io.MetsiConfiguration, **{'run_modes': case})
-
-    def test_mela2_configuration(self):
+    def test_control_configurations(self):
         args = ['cli_input', 'cli_output', 'cli_control.yaml', '-s', 'full', '--state-format', 'vmi12', '--state-output-container', 'json']
-        cli_args = aio.parse_cli_arguments(args)
-        control_args = {'state_output_container': 'pickle', 'preprocessing_output_container': 'json', 'run_modes': 'preprocess'}
-        result = lukefi.metsi.app.app_io.generate_program_configuration(cli_args, control_args)
+        cli_args = parse_cli_arguments(args)
+        control_args = {'state_output_container': 'pickle', 'preprocessing_output_container': 'json', 'run_modes': 'preprocess,simulate'}
+        result = generate_program_configuration(cli_args, control_args)
         self.assertEqual('cli_input', result.input_path)
         self.assertEqual('cli_control.yaml', result.control_file)
         self.assertEqual('cli_output', result.target_directory)
@@ -81,5 +37,42 @@ class TestAppIO(unittest.TestCase):
         self.assertEqual('json', result.state_output_container)  # CLI overrides control source
         self.assertEqual('json', result.preprocessing_output_container)  # control source overrides Mela2Configuration
         self.assertEqual(None, result.derived_data_output_container)  # Mela2Configuration default
-        self.assertEqual([RunMode.PREPROCESS], result.run_modes)
+        self.assertEqual([RunMode.PREPROCESS, RunMode.SIMULATE], result.run_modes)
+
+
+class TestRunModes(unittest.TestCase):
+    def test_valid_typical_run_modes(self):
+        modes = ['preprocess', 'export_prepro', 'simulate', 'postprocess', 'export']
+        result = MetsiConfiguration._validate_and_sort_run_modes(modes)
+        self.assertEqual(result, [RunMode.PREPROCESS, RunMode.EXPORT_PREPRO, RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT])
+
+
+    def test_valid_run_modes_sorted(self):
+        modes = ['simulate', 'PREPROCESS', 'export']
+        result = MetsiConfiguration._validate_and_sort_run_modes(modes)
+        self.assertEqual(result, [RunMode.PREPROCESS, RunMode.SIMULATE, RunMode.EXPORT])
+
+    def test_valid_run_modes_with_dependencies(self):
+        modes = ['simulate', 'postprocess', 'export']
+        result = MetsiConfiguration._validate_and_sort_run_modes(modes)
+        self.assertEqual(result, [RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT])
+
+    def test_invalid_run_mode(self):
+        modes = ['simulate', 'invalid_mode']
+        self.assertRaises(ConfigurationException,
+                          MetsiConfiguration._validate_and_sort_run_modes,
+                          modes)
+
+    def test_export_prepro_without_preprocess(self):
+        modes = ['export_prepro']
+        self.assertRaises(ConfigurationException, 
+                          MetsiConfiguration._validate_and_sort_run_modes,
+                          modes)
+
+    def test_export_without_simulate_or_postprocess(self):
+        modes = ['export']
+        self.assertRaises(ConfigurationException, 
+                          MetsiConfiguration._validate_and_sort_run_modes,
+                          modes)
+
 
