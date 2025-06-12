790c9f0793cb17993fc36bbdb336ca430d37d3f9
Tapani Stylman <tapani.stylman@gofore.com>
2025-06-12 15:38:49 +0300

    fix: fix robot tests to work with control changes

--- tests/robot/SMK_01/input/params.py
+++ tests/robot/SMK_01/input/params.py
@@ -1,72 +1,77 @@
 from tests.robot.SMK_01.input.collectives import collectives
+from lukefi.metsi.domain.pre_ops import *
+from lukefi.metsi.domain.sim_ops import *
+from lukefi.metsi.sim.generators import *
 
-params = { 
-   "first_thinning": [
-       {
-           "thinning_factor": 0.97,
-           "e": 0.2,
-           "dominant_height_lower_bound": 11,
-           "dominant_height_upper_bound": 16
-       }
+
+params = {
+    first_thinning: [
+        {
+            "thinning_factor": 0.97,
+            "e": 0.2,
+            "dominant_height_lower_bound": 11,
+            "dominant_height_upper_bound": 16
+        }
     ],
-    "thinning_from_below": [
-       {
-           "thinning_factor": 0.97,
-           "e": 0.2
-       }
-    ],
-    "thinning_from_above": [
-       {
-           "thinning_factor": 0.98,
-           "e": 0.2
-       }
-    ],
-    "even_thinning": [
-       {
-           "thinning_factor": 0.9,
+    thinning_from_below: [
+        {
+            "thinning_factor": 0.97,
             "e": 0.2
         }
     ],
-    "calculate_biomass": [
-       {"model_set": 1}
+    thinning_from_above: [
+        {
+            "thinning_factor": 0.98,
+            "e": 0.2
+        }
     ],
-    "report_collectives": [ collectives],
-    "report_period": [
-       {"overall_volume": "cross_cutting.volume_per_ha"}
+    even_thinning: [
+        {
+            "thinning_factor": 0.9,
+            "e": 0.2
+        }
     ],
-    "calculate_npv": [
-       {"interest_rates": [1, 2, 3, 4, 5]}
+    calculate_biomass: [
+        {"model_set": 1}
     ],
-    "collect_standing_tree_properties": [
-       {"properties": ["stems_per_ha", "species", "breast_height_diameter", "height", "breast_height_age", "biological_age", "saw_log_volume_reduction_factor"]}
+    report_collectives: [collectives],
+    report_period: [
+        {"overall_volume": "cross_cutting.volume_per_ha"}
     ],
-    "collect_felled_tree_properties": [
-       {"properties": ["stems_per_ha", "species", "breast_height_diameter", "height"]}
+    calculate_npv: [
+        {"interest_rates": [1, 2, 3, 4, 5]}
     ],
-    "planting": [
-       {"tree_count": 10}
+    collect_standing_tree_properties: [
+        {"properties": ["stems_per_ha", "species", "breast_height_diameter", "height",
+                        "breast_height_age", "biological_age", "saw_log_volume_reduction_factor"]}
+    ],
+    collect_felled_tree_properties: [
+        {"properties": ["stems_per_ha", "species", "breast_height_diameter", "height"]}
+    ],
+    planting: [
+        {"tree_count": 10}
     ]
 }
-    
+
 param_files = {
-    "thinning_from_above": {
-       "thinning_limits": "data/parameter_files/Thin.txt"
+    thinning_from_above: {
+        "thinning_limits": "data/parameter_files/Thin.txt"
     },
-    "cross_cut_felled_trees": {
-       "timber_price_table": "data/parameter_files/timber_price_table.csv"
+    cross_cut_felled_trees: {
+        "timber_price_table": "data/parameter_files/timber_price_table.csv"
     },
-    "cross_cut_standing_trees": {
-       "timber_price_table": "data/parameter_files/timber_price_table.csv"
+    cross_cut_standing_trees: {
+        "timber_price_table": "data/parameter_files/timber_price_table.csv"
     },
-    "clearcutting": {
-       "clearcutting_limits_ages": "data/parameter_files/renewal_ages_southernFI.txt",
-       "clearcutting_limits_diameters": "data/parameter_files/renewal_diameters_southernFI.txt"
+    clearcutting: {
+        "clearcutting_limits_ages": "data/parameter_files/renewal_ages_southernFI.txt",
+        "clearcutting_limits_diameters": "data/parameter_files/renewal_diameters_southernFI.txt"
     },
-    "planting": {
-       "planting_instructions": "data/parameter_files/planting_instructions.txt"
+    planting: {
+        "planting_instructions": "data/parameter_files/planting_instructions.txt"
     },
-    "calculate_npv": {
-       "land_values": "data/parameter_files/land_values_per_site_type_and_interest_rate.json",
-       "renewal_costs": "data/parameter_files/renewal_operation_pricing.csv"
+    calculate_npv: {
+        "land_values": "data/parameter_files/land_values_per_site_type_and_interest_rate.json",
+        "renewal_costs": "data/parameter_files/renewal_operation_pricing.csv"
     }
 }
