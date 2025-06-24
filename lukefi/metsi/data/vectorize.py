94cf01a062113757aeaef2939ca192c9c475058f
Tapani Stylman <tapani.stylman@gofore.com>
2025-06-24 18:10:08 +0300

    cleanup: clarify vector datatypes
    - Replaced misleading dtypes with dicts more representative of their actual use
    - Added type hints for ReferenceTrees and Strata attributes
    - Removed unnecessary dataclass decorators
    - Added docstring for vectorize function

--- lukefi/metsi/data/vectorize.py
+++ lukefi/metsi/data/vectorize.py
@@ -1,67 +1,65 @@
-from dataclasses import dataclass
 from typing import Optional, Any
 import numpy as np
 import numpy.typing as npt
 from lukefi.metsi.data.model import ForestStand
 
 
-DTYPE_TREE = np.dtype([
-    ("identifier", "U20"),
-    ("tree_number", np.int32),
-    ("species", np.int32),
-    ("breast_height_diameter", np.float64),
-    ("height", np.float64),
-    ("measured_height", np.float64),
-    ("breast_height_age", np.float64),
-    ("biological_age", np.float64),
-    ("stems_per_ha", np.float64),
-    ("origin", np.int32),
-    ("management_category", np.int32),
-    ("saw_log_volume_reduction_factor", np.float64),
-    ("pruning_year", np.int16),
-    ("age_when_10cm_diameter_at_breast_height", np.int16),
-    ("stand_origin_relative_position", np.float64, (3,)),
-    ("lowest_living_branch_height", np.float64),
-    ("tree_category", np.str_),
-    ("storey", np.int32),
-    ("sapling", np.bool_),
-    ("tree_type", "U20"),
-    ("tuhon_ilmiasu", "U20"),
-    ("latvuskerros", np.float64)  # NOTE: for benchmarking purposes
-])
+DTYPES_TREE: dict[str, npt.DTypeLike] = {
+    "identifier": np.dtype("U20"),
+    "tree_number": np.int32,
+    "species": np.int32,
+    "breast_height_diameter": np.float64,
+    "height": np.float64,
+    "measured_height": np.float64,
+    "breast_height_age": np.float64,
+    "biological_age": np.float64,
+    "stems_per_ha": np.float64,
+    "origin": np.int32,
+    "management_category": np.int32,
+    "saw_log_volume_reduction_factor": np.float64,
+    "pruning_year": np.int16,
+    "age_when_10cm_diameter_at_breast_height": np.int16,
+    "stand_origin_relative_position": np.dtype((np.float64, (3,))),
+    "lowest_living_branch_height": np.float64,
+    "tree_category": np.str_,
+    "storey": np.int32,
+    "sapling": np.bool_,
+    "tree_type": np.dtype("U20"),
+    "tuhon_ilmiasu": np.dtype("U20"),
+    "latvuskerros": np.float64  # NOTE: for benchmarking purposes
+}
 
-DTYPE_STRATA = np.dtype([
-    ("identifier", "U20"),
-    ("species", np.int32),
-    ("mean_diameter", np.float64),
-    ("mean_height", np.float64),
-    ("breast_height_age", np.float64),
-    ("biological_age", np.int32),
-    ("stems_per_ha", np.float64),
-    ("basal_area", np.float64),
-    ("origin", np.int32),
-    ("management_category", np.int32),
-    ("saw_log_volume_reduction_factor", np.float64),
-    ("cutting_year", np.int32),
-    ("age_when_10cm_diameter_at_breast_height", np.int16),
-    ("tree_number", np.int32),
-    ("stand_origin_relative_position", np.float64, (3,)),
-    ("lowest_living_branch_height", np.float64),
-    ("storey", np.int32),
-    ("sapling_stems_per_ha", np.float64),
-    ("sapling_stratum", np.bool_),
-    ("number_of_generated_trees", np.int32)
-])
+DTYPES_STRATA: dict[str, npt.DTypeLike] = {
+    "identifier": np.dtype("U20"),
+    "species": np.int32,
+    "mean_diameter": np.float64,
+    "mean_height": np.float64,
+    "breast_height_age": np.float64,
+    "biological_age": np.int32,
+    "stems_per_ha": np.float64,
+    "basal_area": np.float64,
+    "origin": np.int32,
+    "management_category": np.int32,
+    "saw_log_volume_reduction_factor": np.float64,
+    "cutting_year": np.int32,
+    "age_when_10cm_diameter_at_breast_height": np.int16,
+    "tree_number": np.int32,
+    "stand_origin_relative_position": np.dtype((np.float64, (3,))),
+    "lowest_living_branch_height": np.float64,
+    "storey": np.int32,
+    "sapling_stems_per_ha": np.float64,
+    "sapling_stratum": np.bool_,
+    "number_of_generated_trees": np.int32
+}
 
 
-@dataclass
 class VectorData():
-    def __init__(self, dtype):
-        self.dtype = dtype
+    def __init__(self, dtypes: dict[str, npt.DTypeLike]):
+        self.dtypes = dtypes
 
     def vectorize(self, attr_dict):
         for k, v in attr_dict.items():
-            setattr(self, k, np.array(self.defaultify(v, self.dtype[k]), self.dtype[k]))
+            setattr(self, k, np.array(self.defaultify(v, self.dtypes[k]), self.dtypes[k]))
             if not self.is_contiguous(k):
                 raise Exception("Vectorized data is not contiguous")
         self.set_size(attr_dict)
@@ -105,18 +103,60 @@ class VectorData():
         return value
 
 
-@dataclass
 class ReferenceTrees(VectorData):
 
+    identifier: npt.NDArray[np.str_]
+    tree_number: npt.NDArray[np.int32]
+    species: npt.NDArray[np.int32]
+    breast_height_diameter: npt.NDArray[np.float64]
+    height: npt.NDArray[np.float64]
+    measured_height: npt.NDArray[np.float64]
+    breast_height_age: npt.NDArray[np.float64]
+    biological_age: npt.NDArray[np.float64]
+    stems_per_ha: npt.NDArray[np.float64]
+    origin: npt.NDArray[np.int32]
+    management_category: npt.NDArray[np.int32]
+    saw_log_volume_reduction_factor: npt.NDArray[np.float64]
+    pruning_year: npt.NDArray[np.int16]
+    age_when_10cm_diameter_at_breast_height: npt.NDArray[np.int16]
+    stand_origin_relative_position: npt.NDArray[np.float64]
+    lowest_living_branch_height: npt.NDArray[np.float64]
+    tree_category: npt.NDArray[np.str_]
+    storey: npt.NDArray[np.int32]
+    sapling: npt.NDArray[np.bool_]
+    tree_type: npt.NDArray[np.str_]
+    tuhon_ilmiasu: npt.NDArray[np.str_]
+    latvuskerros: npt.NDArray[np.float64]
+
     def __init__(self):
-        super().__init__(DTYPE_TREE)
+        super().__init__(DTYPES_TREE)
 
 
-@dataclass
 class Strata(VectorData):
 
+    identifier: npt.NDArray[np.str_]
+    species: npt.NDArray[np.int32]
+    mean_diameter: npt.NDArray[np.float64]
+    mean_height: npt.NDArray[np.float64]
+    breast_height_age: npt.NDArray[np.float64]
+    biological_age: npt.NDArray[np.int32]
+    stems_per_ha: npt.NDArray[np.float64]
+    basal_area: npt.NDArray[np.float64]
+    origin: npt.NDArray[np.int32]
+    management_category: npt.NDArray[np.int32]
+    saw_log_volume_reduction_factor: npt.NDArray[np.float64]
+    cutting_year: npt.NDArray[np.int32]
+    age_when_10cm_diameter_at_breast_height: npt.NDArray[np.int16]
+    tree_number: npt.NDArray[np.int32]
+    stand_origin_relative_position: npt.NDArray[np.float64]
+    lowest_living_branch_height: npt.NDArray[np.float64]
+    storey: npt.NDArray[np.int32]
+    sapling_stems_per_ha: npt.NDArray[np.float64]
+    sapling_stratum: npt.NDArray[np.bool_]
+    number_of_generated_trees: npt.NDArray[np.int32]
+
     def __init__(self):
-        super().__init__(DTYPE_STRATA)
+        super().__init__(DTYPES_STRATA)
 
 
 CONTAINERS = {
@@ -126,6 +166,21 @@ CONTAINERS = {
 
 
 def vectorize(stands: list[ForestStand], **operation_params) -> list[ForestStand]:
+    """
+    Modifies a list of ForestStand objects' reference_trees and tree_strata into a struct-of-arrays style.
+    The lists of ReferenceTree and TreeStratum are converted into ReferenceTrees and Strata with numpy arrays for
+    each attribute.
+    
+    Note that this should be the default representation in the future and the conversion from array-of-structs
+    should no longer be necessary.
+    
+    Args:
+        stands (list[ForestStand]): List of ForestStand objects in standard AoS format
+
+    Returns:
+        list[ForestStand]: A reference to the same list is returned after the objects are modified in-place
+    """
+    
     target = operation_params.get('target', None)
     if target is None:
         target = ['reference_trees', 'tree_strata']
