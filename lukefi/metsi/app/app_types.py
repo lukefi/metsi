13be21272692a6de64e19b3223a3b40b6c2cc020
Tapani Stylman <tapani.stylman@gofore.com>
2025-06-26 20:16:42 +0300

    cleanup

--- lukefi/metsi/app/app_types.py
+++ lukefi/metsi/app/app_types.py
@@ -1,15 +1,14 @@
 from dataclasses import dataclass
-from typing import Generic, TypeVar, List, Optional
+from typing import List, Optional
 from lukefi.metsi.data.model import ForestStand
 from lukefi.metsi.sim.core_types import OperationPayload
 
-T = TypeVar("T")
 
 ForestOpPayload = OperationPayload[ForestStand]
 SimResults = dict[str, list[ForestOpPayload]]
 
 @dataclass
-class ExportableContainer(Generic[T]):
+class ExportableContainer[T]:
     """ Output container for application results """
     export_objects: List[T]
     additional_vars: Optional[List[str]]
