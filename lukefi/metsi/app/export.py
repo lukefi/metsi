d92c972ddc9b94a88b5a38fcb089e7dcad44d71c
Tapani Stylman <tapani.stylman@gofore.com>
2025-06-27 11:48:42 +0300

    cleanup

--- lukefi/metsi/app/export.py
+++ lukefi/metsi/app/export.py
@@ -1,5 +1,8 @@
 import copy
 from pathlib import Path
+from functools import partial
+from typing import Callable
+
 
 from lukefi.metsi.app.app_io import MetsiConfiguration
 from lukefi.metsi.app.app_types import SimResults, ExportableContainer
@@ -13,23 +16,20 @@ from lukefi.metsi.sim.runners import evaluate_sequence
 
 
 def export_files(config: MetsiConfiguration, decl: list[dict], data: SimResults):
-    output_handlers = []
+    output_handlers: list[tuple[str, Callable[[], None]]] = []
     for export_module_declaration in decl:
         export_module = export_module_declaration.get("format", None)
         if export_module == "J":
             j_config = parse_j_config(config, export_module_declaration)
-            output_handlers.append((export_module,
-                                    lambda: j_out(data, **j_config)))
+            output_handlers.append((export_module, partial(j_out, data, **j_config)))
         elif export_module == "rm_schedules_events_timber":
             filename1 = export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")
             target_path1 = Path(config.target_directory, filename1)
-            output_handlers.append((export_module,
-                                    lambda: rm_schedules_events_timber(target_path1, data)))
+            output_handlers.append((export_module, partial(rm_schedules_events_timber,target_path1, data)))
         elif export_module == "rm_schedules_events_trees":
             filename2 = export_module_declaration.get("filename", "rm_schedule_trees.txt")
             target_path2 = Path(config.target_directory, filename2)
-            output_handlers.append((export_module,
-                                    lambda: rm_schedules_events_trees(target_path2, data)))
+            output_handlers.append((export_module, partial(rm_schedules_events_trees, target_path2, data)))
         else:
             print_logline(f"Unknown output format for export: '{export_module}'")
     for export_module, handler in output_handlers:
