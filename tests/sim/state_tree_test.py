4db48076c121d24944f55eec879c82f3e1ff59ef
Tapani Stylman <tapani.stylman@gofore.com>
2025-07-02 19:26:48 +0300

    feat: build state tree in EventTree.evaluate

--- tests/sim/state_tree_test.py
+++ tests/sim/state_tree_test.py
@@ -3,7 +3,7 @@ import unittest
 from lukefi.metsi.sim.state_tree import StateTree
 
 
-def testing_operation(op_tuple: tuple[list[int], list[int]], **params) -> tuple[list[int], list[int]]:
+def dummy_operation(op_tuple: tuple[list[int], list[int]], **params) -> tuple[list[int], list[int]]:
     state, collected_data = op_tuple
     for i, _ in enumerate(state):
         state[i] += params.get("increment", 1)
@@ -13,33 +13,38 @@ def testing_operation(op_tuple: tuple[list[int], list[int]], **params) -> tuple[
 
 class StateTreeTest(unittest.TestCase):
     def test_init(self):
-        state = [0, 0, 0]
-        parameters = {"increment": 2, "mult": 2}
-        state_tree = StateTree(state, testing_operation, 1, parameters)
+        state_tree: StateTree[list[int]] = StateTree()
 
-        self.assertListEqual(state_tree.state, state)
-        self.assertEqual(state_tree.done_operation, testing_operation)
-        self.assertEqual(state_tree.time_point, 1)
-        self.assertDictEqual(state_tree.operation_params, parameters)
         self.assertListEqual(state_tree.branches, [])
 
     def test_add_branch(self):
         state = [0, 0, 0]
         parameters = {"increment": 2, "mult": 2}
-        state, _ = testing_operation((state, [0, 0, 0]), increment=2, mult=2)
-        state_tree = StateTree(state, testing_operation, 1, parameters)
+        state, _ = dummy_operation((state, [0, 0, 0]), increment=2, mult=2)
+        state_tree: StateTree[list[int]] = StateTree()
+        state_tree.state = state
+        state_tree.operation_params = parameters
+        state_tree.done_operation = dummy_operation
+        state_tree.time_point = 1
         state = state.copy()
-        state_tree.add_branch(StateTree(testing_operation((state, [0, 0, 0]), increment=2, mult=1)[
-                              0], testing_operation, 2, {"increment": 2, "mult": 1}))
+
+        parameters2 = {"increment": 2, "mult": 1}
+        new_branch: StateTree[list[int]] = StateTree()
+        new_branch.state = dummy_operation((state, [0, 0, 0]), **parameters2)[0]
+        new_branch.operation_params = parameters2
+        new_branch.done_operation = dummy_operation
+        new_branch.time_point = 2
+
+        state_tree.add_branch(new_branch)
 
         self.assertListEqual(state_tree.state, [2, 2, 2])
-        self.assertEqual(state_tree.done_operation, testing_operation)
+        self.assertEqual(state_tree.done_operation, dummy_operation)
         self.assertEqual(state_tree.time_point, 1)
         self.assertDictEqual(state_tree.operation_params, parameters)
         self.assertEqual(len(state_tree.branches), 1)
 
         self.assertListEqual(state_tree.branches[0].state, [4, 4, 4])
-        self.assertEqual(state_tree.branches[0].done_operation, testing_operation)
+        self.assertEqual(state_tree.branches[0].done_operation, dummy_operation)
         self.assertEqual(state_tree.branches[0].time_point, 2)
-        self.assertDictEqual(state_tree.branches[0].operation_params, {"increment": 2, "mult": 1})
+        self.assertDictEqual(state_tree.branches[0].operation_params, parameters2)
         self.assertEqual(len(state_tree.branches[0].branches), 0)
