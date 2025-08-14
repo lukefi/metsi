test, fix: add more robot tests, fix bugs and improve calculation accuracy (#100)
### Added more Robot test cases
- DryRun for a bare minimum smoketest. Runs "python -m
lukefi.metsi.app.lukefi -h" and checks that the process exits
successfully.
- VMI13_mini for running the default control.py with the
VMI13_source_mini dataset and comparing output.

### Re-generated reference output data for Robot tests
- Branched earlier versions (1.2.1, 2.0.2, 2.2.0) and reverted the Njit
performance changes while keeping the y1 -> y1t, etc. fix in
'_taper_curve_correction'.
- Ran the tests and saved output to use as reference

### Replaced linear approximation of integral
$$V =
\frac{\pi}{4}\int_{h_1}^{h_2}\left[\frac{1}{100}\left(ax+bx^2+cx^3+dx^5+ex^8+fx^{13}+gx^{21}+hx^{34}\right)\right]^2
\text{d}\bar{h}$$
$$x = \frac{H-\bar{h}}{H} \Rightarrow \text{d}\bar{h} = -H\text{d}x$$
$$\Rightarrow V = -H
\frac{\pi}{4}\int_{\bar{h}=h_1}^{\bar{h}=h_2}\left[\frac{1}{100}\left(ax+bx^2+cx^3+dx^5+ex^8+fx^{13}+gx^{21}+hx^{34}\right)\right]^2
\text{d}x$$
$$=-H \left.\frac{\pi}{40000}
\left[
\frac{a^2}{3} x^3 +
\frac{ab}{2} x^4 +
\frac{2ac + b^2}{5} x^5 +
\frac{bc}{3} x^6 +
\frac{2ad + c^2}{7} x^7 +
\frac{bd}{4} x^8 +
\frac{2cd}{9} x^9 +
\frac{ae}{5} x^{10} + \right. \right. $$
$$\left. \left.
\frac{2be + d^2}{11} x^{11} +
\frac{ce}{6} x^{12} +
\frac{de}{7} x^{14} +
\frac{2af}{15} x^{15} +
\frac{bf}{8} x^{16} +
\frac{2cf + e^2}{17} x^{17} +
\frac{2df}{19} x^{19} +
\frac{ef}{11} x^{22} +
\frac{2ag}{23} x^{23} + \right. \right. $$
$$\left. \left.
\frac{bg}{12} x^{24} + 
\frac{2cg}{25} x^{25} +
\frac{2dg + f^2}{27} x^{27} +
\frac{eg}{15} x^{30} +
\frac{2fg}{35} x^{35} +
\frac{ah}{18} x^{36} +
\frac{2bh}{37} x^{37} +
\frac{ch}{19} x^{38} + \right. \right. $$
$$\left. \left.
\frac{dh}{20} x^{40} +
\frac{2eh + g^2}{43} x^{43} +
\frac{fh}{24} x^{48} +
\frac{gh}{28} x^{56} +
\frac{h^2}{69} x^{69}
\right]
\right|_{\bar{h}=h_1}^{\bar{h}=h_2}$$
- '_volume' now uses the analytic solution for integral when 

lukefi/metsi/forestry/cross_cutting/stem_profile.py
@@ -167,6 +167,82 @@ def _ghat(h: float, height: int, coef: np.ndarray) -> float:
     d = d / 100.0
     return (d * d) * np.pi / 4.0
 
+@njit(fastmath=True)
+def _V(h_: float, height: int, coef: np.ndarray) -> float:
+    x = (height - h_) / height
+    # h = -(x * height - height) = height - x * height
+    # dh / dx = -height
+    x2 = x * x
+    x3 = x * x2
+    x4 = x2 * x2
+    x5 = x2 * x3
+    x6 = x3 * x3
+    x7 = x3 * x4
+    x8 = x4 * x4
+    x9 = x4 * x5
+    x10 = x5 * x5
+    x11 = x5 * x6
+    x12 = x6 * x6
+    x14 = x7 * x7
+    x15 = x7 * x8
+    x16 = x8 * x8
+    x17 = x8 * x9
+    x19 = x9 * x10
+    x22 = x11 * x11
+    x23 = x11 * x12
+    x24 = x12 * x12
+    x25 = x14 * x11
+    x27 = x11 * x16
+    x30 = x15 * x15
+    x35 = x23 * x12
+    x36 = x17 * x19
+    x37 = x15 * x22
+    x38 = x19 * x19
+    x40 = x17 * x23
+    x43 = x19 * x24
+    x48 = x24 * x24
+    x56 = x37 * x19
+    x69 = x23 * x23 * x23
+
+    a = coef[0]
+    b = coef[1]
+    c = coef[2]
+    d = coef[3]
+    e = coef[4]
+    f = coef[5]
+    g = coef[6]
+    h = coef[7]
+
+    return -height * np.pi / 40000 * ((a * a) / 3 * x3 +
+                                      (a * b) / 2 * x4 +
+                                      (2 * a * c + b * b) / 5 * x5 +
+                                      (b * c) / 3 * x6 +
+                                      (2 * a * d + c * c) / 7 * x7 +
+                                      (b * d) / 4 * x8 +
+                                      (c * d) * 2 / 9 * x9 +
+                                      (a * e) / 5 * x10 +
+                                      (2 * b * e + d * d) / 11 * x11 +
+                                      (c * e) / 6 * x12 +
+                                      (d * e) / 7 * x14 +
+                                      (a * f) * 2 / 15 * x15 +
+                                      (b * f) / 8 * x16 +
+                                      (2 * c * f + e * e) / 17 * x17 +
+                                      (d * f) * 2 / 19 * x19 +
+                                      (e * f) / 11 * x22 +
+                                      (a * g) * 2 / 23 * x23 +
+                                      (b * g) / 12 * x24 +
+                                      (c * g) * 2 / 25 * x25 +
+                                      (2 * d * g + f * f) / 27 * x27 +
+                                      (e * g) / 15 * x30 +
+                                      (f * g) * 2 / 35 * x35 +
+                                      (a * h) / 18 * x36 +
+                                      (b * h) * 2 / 37 * x37 +
+                                      (c * h) / 19 * x38 +
+                                      (d * h) / 20 * x40 +
+                                      (2 * e * h + g * g) / 43 * x43 +
+                                      (f * h) / 24 * x48 +
+                                      (g * h) / 28 * x56 +
+                                      (h * h) / 69 * x69)
 
 @njit
 def _volume(hkanto: float, height: int, coeff: np.ndarray):
@@ -182,10 +258,9 @@ def _volume(hkanto: float, height: int, coeff: np.ndarray):
 
     for j, _ in enumerate(v_piece):
         x0 = h[j]
-        x1 = h[j + 1]
-        y0 = _ghat(x0, height, coeff)
-        y1 = _ghat(x1, height, coeff)
-        v_piece[j] = (y0 + y1) * (x1 - x0) / 2
+        x1 = h[j+1]
+
+        v_piece[j] = _V(x1, height, coeff) - _V(x0, height, coeff)
 
     h_piece = h[1:]
     v_cum = np.cumsum(v_piece)