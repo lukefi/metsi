b50edd2fe6a1d67232a2926a9729b687a5207174
Tapani Stylman <tapani.stylman@gofore.com>
2025-09-09 13:31:31 +0300

    add more treatment helper classes

--- lukefi/metsi/domain/treatments.py
+++ lukefi/metsi/domain/treatments.py
@@ -11,8 +11,10 @@ from lukefi.metsi.domain.data_collection.marshalling import (
 from lukefi.metsi.domain.data_collection.net_present_value import calculate_npv
 from lukefi.metsi.domain.forestry_operations.clearcut import clearcutting
 from lukefi.metsi.domain.forestry_operations.planting import planting
-from lukefi.metsi.domain.forestry_operations.thinning import even_thinning, first_thinning
+from lukefi.metsi.domain.forestry_operations.thinning import (
+    even_thinning, first_thinning, thinning_from_above, thinning_from_below)
 from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta
+from lukefi.metsi.domain.natural_processes.grow_metsi import grow_metsi
 from lukefi.metsi.sim.event import Condition
 from lukefi.metsi.sim.generators import Treatment
 from lukefi.metsi.sim.operations import do_nothing
@@ -192,6 +194,42 @@ class GrowActa(Treatment[ForestStand]):
                  file_parameters: Optional[dict[str, str]] = None,
                  run_constraints: Optional[dict[str, Any]] = None) -> None:
         super().__init__(treatment_fn=grow_acta,
+                         parameters=parameters,
+                         conditions=conditions,
+                         file_parameters=file_parameters,
+                         run_constraints=run_constraints)
+
+
+class GrowMetsi(Treatment[ForestStand]):
+    def __init__(self, parameters: Optional[dict[str, Any]] = None,
+                 conditions: Optional[list[Condition[ForestStand]]] = None,
+                 file_parameters: Optional[dict[str, str]] = None,
+                 run_constraints: Optional[dict[str, Any]] = None) -> None:
+        super().__init__(treatment_fn=grow_metsi,
+                         parameters=parameters,
+                         conditions=conditions,
+                         file_parameters=file_parameters,
+                         run_constraints=run_constraints)
+
+
+class ThinningFromBelow(Treatment[ForestStand]):
+    def __init__(self, parameters: Optional[dict[str, Any]] = None,
+                 conditions: Optional[list[Condition[ForestStand]]] = None,
+                 file_parameters: Optional[dict[str, str]] = None,
+                 run_constraints: Optional[dict[str, Any]] = None) -> None:
+        super().__init__(treatment_fn=thinning_from_below,
+                         parameters=parameters,
+                         conditions=conditions,
+                         file_parameters=file_parameters,
+                         run_constraints=run_constraints)
+
+
+class ThinningFromAbove(Treatment[ForestStand]):
+    def __init__(self, parameters: Optional[dict[str, Any]] = None,
+                 conditions: Optional[list[Condition[ForestStand]]] = None,
+                 file_parameters: Optional[dict[str, str]] = None,
+                 run_constraints: Optional[dict[str, Any]] = None) -> None:
+        super().__init__(treatment_fn=thinning_from_above,
                          parameters=parameters,
                          conditions=conditions,
                          file_parameters=file_parameters,
