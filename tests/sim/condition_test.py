ca7fafdd8f56c41ed1b123c4bd9e4207dce8254b
Tapani Stylman <tapani.stylman@gofore.com>
2025-09-11 16:40:48 +0300

    test: add unit test for Condition combinations

---
+++ tests/sim/condition_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from lukefi.metsi.sim.condition import Condition
+
+
+class ConditionTest(unittest.TestCase):
+    def test_condition_combinations(self):
+        c1 = Condition[int, int](lambda t, _: t >= 2)
+        c2 = Condition[int, int](lambda _, x: x < 5)
+
+        c_and = c1 & c2
+        c_or = c1 | c2
+
+        self.assertTrue(c_and(2, 4))
+        self.assertFalse(c_and(1, 4))
+        self.assertFalse(c_and(2, 5))
+        self.assertFalse(c_and(1, 6))
+
+        self.assertTrue(c_or(3, 4))
+        self.assertTrue(c_or(1, 3))
+        self.assertTrue(c_or(5, 6))
+        self.assertFalse(c_or(1, 6))
