0e69c54f7c2b5f2a31e006c34d49abf2ba7f19ea
Tapani Stylman <tapani.stylman@gofore.com>
2025-09-18 10:04:43 +0300

    test: add unit test for grow_acta_vectorized

--- tests/domain/growth_operations_test.py
+++ tests/domain/growth_operations_test.py
@@ -1,6 +1,10 @@
 import unittest
+import numpy as np
 
-from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta
+from lukefi.metsi.data.model import ForestStand
+from lukefi.metsi.data.vectorize import vectorize
+from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta, grow_acta_vectorized
+from lukefi.metsi.sim.core_types import CollectedData
 from tests.test_utils import prepare_growth_test_stand
 
 
@@ -19,6 +23,12 @@ class GrowthOperationsTest(unittest.TestCase):
             self.assertTrue(res.stems_per_ha <= ref.stems_per_ha)
             self.assertTrue(res.breast_height_diameter >= ref.breast_height_diameter)
             self.assertTrue(res.height >= ref.height)
+    
+    def assert_domain_sensibility_vectorized(self, stand: ForestStand):
+        fixture = vectorize([prepare_growth_test_stand()])[0]
+        self.assertTrue(np.all(np.less_equal(stand.reference_trees_soa.stems_per_ha , fixture.reference_trees_soa.stems_per_ha)))
+        self.assertTrue(np.all(np.greater_equal(stand.reference_trees_soa.breast_height_diameter , fixture.reference_trees_soa.breast_height_diameter)))
+        self.assertTrue(np.all(np.greater_equal(stand.reference_trees_soa.height , fixture.reference_trees_soa.height)))
 
     def test_grow_acta(self):
         stand = prepare_growth_test_stand()
@@ -31,4 +41,18 @@ class GrowthOperationsTest(unittest.TestCase):
         self.assertEqual(stand.reference_trees[0].breast_height_age, 15)
         self.assertEqual(stand.reference_trees[1].breast_height_age, 15)
         self.assertEqual(stand.reference_trees[2].breast_height_age, 6)
+        self.assertEqual(stand.year, 2030)
+
+    def test_grow_acta_vectorized(self):
+        stand = prepare_growth_test_stand()
+        stand = vectorize([stand])[0]
+        grow_acta_vectorized((stand, CollectedData()))
+        self.assert_domain_sensibility_vectorized(stand)
+        self.assertFalse(stand.reference_trees_soa.sapling[2])
+        self.assertEqual(stand.reference_trees_soa.biological_age[0], 60)
+        self.assertEqual(stand.reference_trees_soa.biological_age[1], 42)
+        self.assertEqual(stand.reference_trees_soa.biological_age[2], 6)
+        self.assertEqual(stand.reference_trees_soa.breast_height_age[0], 15)
+        self.assertEqual(stand.reference_trees_soa.breast_height_age[1], 15)
+        self.assertEqual(stand.reference_trees_soa.breast_height_age[2], 6)
         self.assertEqual(stand.year, 2030)
