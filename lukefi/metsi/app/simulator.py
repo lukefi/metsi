lukefi/metsi

commit 4865444ad5e120c8bcdf998f3b00dd897a7ec1d4

fix: vectorized simulation fixes, #MELA2-92, #MELA2-93 (#122)
## Fixes and small features related to vectorized state handling
- Restored simulation state output functionality
- Implemented RST and CSV output for vectorized data
- Fixed simulation algorithm for vectorized data
- Fixed grow_acta_vectorized

## Fixed state output for non-vectorized data
- Methods such as as_rst_row() and as_internal_csv_row() were not
working properly as they ignored any modifications made on the
LayeredObject in the payload

lukefi/metsi/app/simulator.py
@@ -1,5 +1,5 @@
 from typing import Any
-from lukefi.metsi.data.layered_model import LayeredObject
+from lukefi.metsi.data.layered_model import LayeredObject, PossiblyLayered
 from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeStratum
 
 from lukefi.metsi.app.app_io import MetsiConfiguration
@@ -27,14 +27,24 @@ def run_stands(stands: StandList,
 
     retval: dict[str, list[ForestOpPayload]] = {}
     for stand in stands:
-        overlaid_stand = LayeredObject[ForestStand](stand)
-        overlaid_stand.reference_trees = [LayeredObject[ReferenceTree](tree) for tree in overlaid_stand.reference_trees]
-        overlaid_stand.tree_strata = [LayeredObject[TreeStratum](stratum) for stratum in overlaid_stand.tree_strata]
+        overlaid_stand: PossiblyLayered[ForestStand]
+        if stand.reference_trees_soa is None or stand.tree_strata_soa is None:
+            # If the state is not vectorized, wrap it as a LayeredObject so that new nodes in the EventTree don't have 
+            # to copy the entire state in memory and can just store the data that has actually changed instead.
+            # This is not necessary for vectorized data since similar functionality is provided by the finalize method.
+            overlaid_stand = LayeredObject[ForestStand](stand)
+            overlaid_stand.reference_trees = [LayeredObject[ReferenceTree]
+                                              (tree) for tree in overlaid_stand.reference_trees]
+            overlaid_stand.tree_strata = [LayeredObject[TreeStratum](stratum) for stratum in overlaid_stand.tree_strata]
+        else:
+            overlaid_stand = stand
+
         payload = ForestOpPayload(
             computational_unit=overlaid_stand,
             collected_data=CollectedData(initial_time_point=config.time_points[0]),
             operation_history=[],
         )
+
         schedule_payloads = runner(payload, config, evaluator)
         identifier = stand.identifier
         print_logline(f"Alternatives for stand {identifier}: {len(schedule_payloads)}")

lukefi/metsi/data/formats/io_utils.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 from lukefi.metsi.app.app_types import ExportableContainer
 from lukefi.metsi.data.formats.util import parse_float
 from lukefi.metsi.data.layered_model import PossiblyLayered
-from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeStratum
+from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeStratum, stand_as_internal_csv_row, stand_as_rst_row, stratum_as_rsts_row, tree_as_internal_csv_row, tree_as_rst_row
 from lukefi.metsi.data.formats.rst_const import MSBInitialDataRecordConst as msb_meta
 from lukefi.metsi.domain.forestry_types import StandList
 
@@ -36,7 +36,8 @@ def msb_metadata(stand: PossiblyLayered[ForestStand]) -> tuple[list[str], list[s
         msb_meta.logical_record_metadata_length,
         msb_meta.stand_record_length,
         msb_meta.logical_subrecord_metadata_length,
-        len(stand.reference_trees) * msb_meta.tree_record_length
+        (len(stand.reference_trees) if stand.reference_trees_soa is None else stand.reference_trees_soa.size) *
+        msb_meta.tree_record_length
     ])
     physical_record_metadata = [
         rst_float(outputtable_id),  # UID
@@ -51,7 +52,7 @@ def msb_metadata(stand: PossiblyLayered[ForestStand]) -> tuple[list[str], list[s
         rst_float(msb_meta.stand_record_length)
     ]
     logical_subrecord_metadata = [
-        rst_float(len(stand.reference_trees)),
+        rst_float(len(stand.reference_trees) if stand.reference_trees_soa is None else stand.reference_trees_soa.size),
         rst_float(msb_meta.tree_record_length)
     ]
     return physical_record_metadata, logical_record_metadata, logical_subrecord_metadata
@@ -84,12 +85,16 @@ def rst_forest_stand_rows(stand: PossiblyLayered[ForestStand], additional_vars: 
     result.append(" ".join(chain(
         msb_preliminary_records[0],
         msb_preliminary_records[1],
-        map(rst_float, stand.as_rst_row()),
+        map(rst_float, stand_as_rst_row(stand)),
         msb_preliminary_records[2]
     )))
     # Reference tree row(s)
-    for tree in stand.reference_trees:
-        result.append(" ".join(map(rst_float, tree.as_rst_row())))
+    if stand.reference_trees_soa is not None:
+        for i in range(stand.reference_trees_soa.size):
+            result.append(" ".join(map(rst_float, stand.reference_trees_soa.as_rst_row(i))))
+    else:
+        for tree in stand.reference_trees:
+            result.append(" ".join(map(rst_float, tree_as_rst_row(tree))))
     return result
 
 
@@ -98,10 +103,10 @@ def rsts_forest_stand_rows(stand: PossiblyLayered[ForestStand]) -> list[str]:
     result = []
     result.append(" ".join(chain(
         [str(parse_float(stand.identifier) or stand.stand_id)],
-        map(rst_float, stand.as_rst_row())
+        map(rst_float, stand_as_rst_row(stand))
     )))
     for stratum in stand.tree_strata:
-        result.append(" ".join(map(rst_float, stratum.as_rsts_row())))
+        result.append(" ".join(map(rst_float, stratum_as_rsts_row(stratum))))
     return result
 
 
@@ -115,23 +120,32 @@ def stand_to_csv_rows(stand: PossiblyLayered[ForestStand], delimeter: str,
                       additional_vars: Optional[list[str]]) -> list[str]:
     """converts the :stand:, its reference trees and tree strata to csv rows."""
     result = []
-    result.append(delimeter.join(map(csv_value, stand.as_internal_csv_row(additional_vars))))
-    result.extend(
-        map(
-            lambda tree: delimeter.join(
-                map(
-                    csv_value,
-                    tree.as_internal_csv_row())),
-            stand.reference_trees)
-    )
-    result.extend(
-        map(
-            lambda stratum: delimeter.join(
-                map(
-                    csv_value,
-                    stratum.as_internal_csv_row())),
-            stand.tree_strata)
-    )
+    result.append(delimeter.join(map(csv_value, stand_as_internal_csv_row(stand, additional_vars))))
+    if stand.reference_trees_soa is None:
+        result.extend(
+            map(
+                lambda tree: delimeter.join(
+                    map(
+                        csv_value,
+                        tree_as_internal_csv_row(tree))),
+                stand.reference_trees)
+        )
+    else:
+        for i in range(stand.reference_trees_soa.size):
+            result.append(delimeter.join(map(csv_value, stand.reference_trees_soa.as_internal_csv_row(i))))
+
+    if stand.tree_strata_soa is None:
+        result.extend(
+            map(
+                lambda stratum: delimeter.join(
+                    map(
+                        csv_value,
+                        stratum.as_internal_csv_row())),
+                stand.tree_strata)
+        )
+    else:
+        for i in range(stand.tree_strata_soa.size):
+            result.append(delimeter.join(map(csv_value, stand.tree_strata_soa.as_internal_csv_row(i))))
     return result
 
 

lukefi/metsi/data/vectorize.py
@@ -47,7 +47,7 @@ def vectorize(stands: StandList, **operation_params) -> StandList:
                 raise MetsiException(f"Unknown target type '{t}'")
             setattr(stand, f"{t}_soa", container_obj().vectorize(attr_dict))
             delattr(stand, t)
-
+        stand.vectorized = True
     return stands
 
 

pyproject.toml
@@ -5,7 +5,7 @@ build-backend = "setuptools.build_meta"
 [project]
 name = "lukefi.metsi"
 description = "Metsi forestry simulator."
-version = "3.2.0"
+version = "3.2.1"
 readme = "README.md"
 requires-python = ">=3.12"
 authors = [

lukefi/metsi/app/app_io.py
@@ -27,7 +27,7 @@ class MetsiConfiguration(SimpleNamespace):
     run_modes = [RunMode.PREPROCESS, RunMode.EXPORT_PREPRO, RunMode.SIMULATE, RunMode.POSTPROCESS, RunMode.EXPORT]
     state_format = StateFormat.FDM
     state_input_container = StateInputFormat.CSV
-    state_output_container: Optional[str] = None
+    state_output_container: Optional[StateOutputFormat] = None
     derived_data_output_container: Optional[str] = None
     formation_strategy = FormationStrategy.PARTIAL
     evaluation_strategy = EvaluationStrategy.DEPTH

lukefi/metsi/app/file_io.py
@@ -259,10 +259,10 @@ def write_full_simulation_result_dirtree(result: SimResults, app_arguments: Mets
         for i, schedule in enumerate(schedules):
             if app_arguments.state_output_container is not None:
                 schedule_dir = prepare_target_directory(f"{app_arguments.target_directory}/{stand_id}/{i}")
-                filepath = determine_file_path(schedule_dir, app_arguments.state_output_container)
+                filepath = determine_file_path(schedule_dir, f"sim_result.{app_arguments.state_output_container.value}")
                 write_stands_to_file(ExportableContainer([schedule.computational_unit], None),
                                      filepath,
-                                     app_arguments.state_output_container)
+                                     app_arguments.state_output_container.value)
             if app_arguments.derived_data_output_container is not None:
                 schedule_dir = prepare_target_directory(f"{app_arguments.target_directory}/{stand_id}/{i}")
                 filepath = determine_file_path(schedule_dir, app_arguments.derived_data_output_container)

lukefi/metsi/app/metsi_enum.py
@@ -104,6 +104,7 @@ class StateOutputFormat(StringConfigEnum):
     PICKLE = 'pickle'
     JSON = 'json'
     CSV = 'csv'
+    RST = 'rst'
 
 
 class DerivedDataOutputFormat(StringConfigEnum):

lukefi/metsi/data/model.py
@@ -1,13 +1,14 @@
 import dataclasses
-from typing import Optional
+from typing import Optional, override
 from dataclasses import dataclass
 from lukefi.metsi.app.utils import MetsiException
 from lukefi.metsi.data.enums.internal import (LandUseCategory, OwnerCategory, SiteType, SoilPeatlandCategory,
                                               TreeSpecies, DrainageCategory, Storey)
 from lukefi.metsi.data.enums.mela import MelaLandUseCategory
 from lukefi.metsi.data.formats.util import convert_str_to_type as conv
-from lukefi.metsi.data.layered_model import LayeredObject
+from lukefi.metsi.data.layered_model import LayeredObject, PossiblyLayered
 from lukefi.metsi.data.vector_model import ReferenceTrees, Strata
+from lukefi.metsi.sim.finalizable import Finalizable
 
 # NOTE:
 # * the deepcopy methods here are roughly equivalent to
@@ -208,22 +209,6 @@ class TreeStratum():
         result.storey = Storey(int(row[21])) if row[21] != "None" else None
         return result
 
-    def as_rsts_row(self):
-        return [
-            self.tree_number,
-            self.species,
-            self.origin,
-            self.stems_per_ha,
-            self.mean_diameter,
-            self.mean_height,
-            self.breast_height_age,
-            self.biological_age,
-            self.basal_area,
-            self.sapling_stems_per_ha,
-            self.storey,
-            self.number_of_generated_trees
-        ]
-
 
 @dataclass(init=True, repr=False, order=False, unsafe_hash=False, frozen=False, match_args=False, kw_only=False,
            slots=False, weakref_slot=False, eq=False)
@@ -312,34 +297,6 @@ class ReferenceTree():
             return True
         return False
 
-    def as_internal_csv_row(self) -> list[str]:
-        return [
-            "tree",
-            str(self.identifier),
-            str(self.species),
-            str(self.origin),
-            str(self.stems_per_ha),
-            str(self.breast_height_diameter),
-            str(self.height),
-            str(self.measured_height),
-            str(self.breast_height_age),
-            str(self.biological_age),
-            str(self.saw_log_volume_reduction_factor),
-            str(self.pruning_year),
-            str(self.age_when_10cm_diameter_at_breast_height),
-            str(self.tree_number),
-            str(self.stand_origin_relative_position[0]),
-            str(self.stand_origin_relative_position[1]),
-            str(self.stand_origin_relative_position[2]),
-            str(self.lowest_living_branch_height),
-            str(self.management_category),
-            str(self.tree_category),
-            str(self.sapling),
-            str(self.storey),
-            str(self.tree_type),
-            str(self.tuhon_ilmiasu)
-        ]
-
     @classmethod
     def from_csv_row(cls, row) -> "ReferenceTree":
         result = cls()
@@ -370,31 +327,10 @@ class ReferenceTree():
         result.tuhon_ilmiasu = conv(row[23], str)
         return result
 
-    def as_rst_row(self):
-        return [
-            self.stems_per_ha,
-            self.species,
-            self.breast_height_diameter,
-            self.height,
-            self.breast_height_age,
-            self.biological_age,
-            self.saw_log_volume_reduction_factor,
-            self.pruning_year,
-            self.age_when_10cm_diameter_at_breast_height,
-            self.origin,
-            self.tree_number,
-            self.stand_origin_relative_position[0],
-            self.stand_origin_relative_position[1],
-            self.stand_origin_relative_position[2],
-            self.lowest_living_branch_height,
-            self.management_category,
-            None,
-        ]
-
 
 @dataclass(init=True, repr=False, order=False, unsafe_hash=False, frozen=False, match_args=False, kw_only=False,
            slots=False, weakref_slot=False, eq=False)
-class ForestStand():
+class ForestStand(Finalizable):
     # VMI data type 1
     # SMK data type Stand
 
@@ -466,12 +402,16 @@ class ForestStand():
     def __deepcopy__(self, memo: dict) -> 'ForestStand':
         stand = ForestStand.__new__(ForestStand)
         stand.__dict__.update(self.__dict__)
-        stand.reference_trees = [t.__deepcopy__(memo) for t in stand.reference_trees]
-        stand.tree_strata = [s.__deepcopy__(memo) for s in stand.tree_strata]
-        if stand.monthly_temperatures is not None:
-            stand.monthly_temperatures = list(stand.monthly_temperatures)
-        if stand.monthly_rainfall is not None:
-            stand.monthly_rainfall = list(stand.monthly_rainfall)
+        if self.reference_trees_soa is None or self.tree_strata_soa is None:
+            stand.reference_trees = [t.__deepcopy__(memo) for t in stand.reference_trees]
+            stand.tree_strata = [s.__deepcopy__(memo) for s in stand.tree_strata]
+        else:
+            stand.reference_trees_soa = self.reference_trees_soa.finalize()
+            stand.tree_strata_soa = self.tree_strata_soa.finalize()
+        if self.monthly_temperatures is not None:
+            stand.monthly_temperatures = list(self.monthly_temperatures)
+        if self.monthly_rainfall is not None:
+            stand.monthly_rainfall = list(self.monthly_rainfall)
         return stand
 
     def __hash__(self):
@@ -523,56 +463,6 @@ class ForestStand():
     def has_strata(self):
         return len(self.tree_strata) > 0
 
-    def as_internal_csv_row(self, decl_keys: Optional[list[str]] = None) -> list[str]:
-        result = ["stand", self.identifier]
-        result.extend(self.as_internal_row())
-        if decl_keys is not None:
-            result.extend(self.get_value_list(decl_keys))
-        return result
-
-    def as_internal_row(self):
-        return [
-            self.management_unit_id,
-            self.year,
-            self.area,
-            self.area_weight,
-            self.geo_location[0] if self.geo_location is not None else None,
-            self.geo_location[1] if self.geo_location is not None else None,
-            self.geo_location[2] if self.geo_location is not None else None,
-            self.geo_location[3] if self.geo_location is not None else None,
-            self.degree_days,
-            self.owner_category,
-            self.land_use_category,
-            self.soil_peatland_category,
-            self.site_type_category,
-            self.tax_class_reduction,
-            self.tax_class,
-            self.drainage_category,
-            self.drainage_feasibility,
-            self.drainage_year,
-            self.fertilization_year,
-            self.soil_surface_preparation_year,
-            self.natural_regeneration_feasibility,
-            self.regeneration_area_cleaning_year,
-            self.development_class,
-            self.artificial_regeneration_year,
-            self.young_stand_tending_year,
-            self.pruning_year,
-            self.cutting_year,
-            self.forestry_centre_id,
-            self.forest_management_category,
-            self.method_of_last_cutting,
-            self.municipality_id,
-            self.fra_category,
-            self.land_use_category_detail,
-            self.auxiliary_stand,
-            self.area_weight_factors[0],
-            self.area_weight_factors[1],
-            self.stand_id,
-            self.basal_area,
-            self.dominant_storey_age
-        ]
-
     def from_row(self, row):
         self.management_unit_id = conv(row[0], int)
         self.year = conv(row[1], int)
@@ -622,44 +512,6 @@ class ForestStand():
         stand.from_row(row[2:])
         return stand
 
-    def as_rst_row(self):
-        return [
-            self.management_unit_id,
-            self.year,
-            self.area,
-            self.area_weight,
-            self.geo_location[0] if self.geo_location else None,
-            self.geo_location[1] if self.geo_location else None,
-            self.stand_id,
-            self.geo_location[2] if self.geo_location else None,
-            self.degree_days,
-            self.owner_category.value if self.owner_category else None,
-            self.land_use_category.value if self.land_use_category else None,
-            self.soil_peatland_category,
-            self.site_type_category,
-            self.tax_class_reduction,
-            self.tax_class,
-            self.drainage_category,
-            self.drainage_feasibility,
-            None,
-            self.drainage_year,
-            self.fertilization_year,
-            self.soil_surface_preparation_year,
-            self.natural_regeneration_feasibility,
-            self.regeneration_area_cleaning_year,
-            self.development_class,
-            self.artificial_regeneration_year,
-            self.young_stand_tending_year,
-            self.pruning_year,
-            self.cutting_year,
-            self.forestry_centre_id,
-            self.forest_management_category,
-            self.method_of_last_cutting,
-            self.municipality_id,
-            None,
-            self.dominant_storey_age,
-        ]
-
     def get_value_list(self, keys: Optional[list[str]] = None) -> list:
         """ Returns instance values as list based on keys.
             If keys are not present all attribute values are returned """
@@ -667,6 +519,12 @@ class ForestStand():
         if keys is not None:
             ad = [getattr(self, k) for k in keys]  # Needs to fail noisy
         return ad
+
+    @override
+    def finalize(self):
+        if self.reference_trees_soa is not None and self.tree_strata_soa is not None:
+            self.reference_trees_soa = self.reference_trees_soa.finalize()
+            self.tree_strata_soa = self.tree_strata_soa.finalize()
 
 
 def create_layered_tree(**kwargs) -> LayeredObject[ReferenceTree]:
@@ -691,3 +549,189 @@ def create_layered_stratum(**kwargs) -> LayeredObject[TreeStratum]:
     for k, v in kwargs.items():
         setattr(layered, k, v)
     return layered
+
+
+def stand_as_internal_csv_row(stand: PossiblyLayered[ForestStand], decl_keys: Optional[list[str]] = None) -> list[str]:
+    result = ["stand", stand.identifier]
+    result.extend(stand_as_internal_row(stand))
+    if decl_keys is not None:
+        result.extend(stand.get_value_list(decl_keys))
+    return result
+
+
+def tree_as_internal_csv_row(tree: PossiblyLayered[ReferenceTree]) -> list[str]:
+    return [
+        "tree",
+        str(tree.identifier),
+        str(tree.species),
+        str(tree.origin),
+        str(tree.stems_per_ha),
+        str(tree.breast_height_diameter),
+        str(tree.height),
+        str(tree.measured_height),
+        str(tree.breast_height_age),
+        str(tree.biological_age),
+        str(tree.saw_log_volume_reduction_factor),
+        str(tree.pruning_year),
+        str(tree.age_when_10cm_diameter_at_breast_height),
+        str(tree.tree_number),
+        str(tree.stand_origin_relative_position[0]),
+        str(tree.stand_origin_relative_position[1]),
+        str(tree.stand_origin_relative_position[2]),
+        str(tree.lowest_living_branch_height),
+        str(tree.management_category),
+        str(tree.tree_category),
+        str(tree.sapling),
+        str(tree.storey),
+        str(tree.tree_type),
+        str(tree.tuhon_ilmiasu)
+    ]
+
+
+def stratum_as_internal_csv_row(stratum: PossiblyLayered[TreeStratum]) -> list[str]:
+    return [
+        "stratum",
+        str(stratum.identifier),
+        str(stratum.species),
+        str(stratum.origin),
+        str(stratum.stems_per_ha),
+        str(stratum.mean_diameter),
+        str(stratum.mean_height),
+        str(stratum.breast_height_age),
+        str(stratum.biological_age),
+        str(stratum.basal_area),
+        str(stratum.saw_log_volume_reduction_factor),
+        str(stratum.cutting_year),
+        str(stratum.age_when_10cm_diameter_at_breast_height),
+        str(stratum.tree_number),
+        str(stratum.stand_origin_relative_position[0]),
+        str(stratum.stand_origin_relative_position[1]),
+        str(stratum.stand_origin_relative_position[2]),
+        str(stratum.lowest_living_branch_height),
+        str(stratum.management_category),
+        str(stratum.sapling_stems_per_ha),
+        str(stratum.sapling_stratum),
+        str(stratum.storey)
+    ]
+
+
+def stand_as_rst_row(stand: PossiblyLayered[ForestStand]):
+    return [
+        stand.management_unit_id,
+        stand.year,
+        stand.area,
+        stand.area_weight,
+        stand.geo_location[0] if stand.geo_location else None,
+        stand.geo_location[1] if stand.geo_location else None,
+        stand.stand_id,
+        stand.geo_location[2] if stand.geo_location else None,
+        stand.degree_days,
+        stand.owner_category.value if stand.owner_category else None,
+        stand.land_use_category.value if stand.land_use_category else None,
+        stand.soil_peatland_category,
+        stand.site_type_category,
+        stand.tax_class_reduction,
+        stand.tax_class,
+        stand.drainage_category,
+        stand.drainage_feasibility,
+        None,
+        stand.drainage_year,
+        stand.fertilization_year,
+        stand.soil_surface_preparation_year,
+        stand.natural_regeneration_feasibility,
+        stand.regeneration_area_cleaning_year,
+        stand.development_class,
+        stand.artificial_regeneration_year,
+        stand.young_stand_tending_year,
+        stand.pruning_year,
+        stand.cutting_year,
+        stand.forestry_centre_id,
+        stand.forest_management_category,
+        stand.method_of_last_cutting,
+        stand.municipality_id,
+        None,
+        stand.dominant_storey_age,
+    ]
+
+
+def stand_as_internal_row(stand: PossiblyLayered[ForestStand]):
+    return [
+        stand.management_unit_id,
+        stand.year,
+        stand.area,
+        stand.area_weight,
+        stand.geo_location[0] if stand.geo_location is not None else None,
+        stand.geo_location[1] if stand.geo_location is not None else None,
+        stand.geo_location[2] if stand.geo_location is not None else None,
+        stand.geo_location[3] if stand.geo_location is not None else None,
+        stand.degree_days,
+        stand.owner_category,
+        stand.land_use_category,
+        stand.soil_peatland_category,
+        stand.site_type_category,
+        stand.tax_class_reduction,
+        stand.tax_class,
+        stand.drainage_category,
+        stand.drainage_feasibility,
+        stand.drainage_year,
+        stand.fertilization_year,
+        stand.soil_surface_preparation_year,
+        stand.natural_regeneration_feasibility,
+        stand.regeneration_area_cleaning_year,
+        stand.development_class,
+        stand.artificial_regeneration_year,
+        stand.young_stand_tending_year,
+        stand.pruning_year,
+        stand.cutting_year,
+        stand.forestry_centre_id,
+        stand.forest_management_category,
+        stand.method_of_last_cutting,
+        stand.municipality_id,
+        stand.fra_category,
+        stand.land_use_category_detail,
+        stand.auxiliary_stand,
+        stand.area_weight_factors[0],
+        stand.area_weight_factors[1],
+        stand.stand_id,
+        stand.basal_area,
+        stand.dominant_storey_age
+    ]
+
+
+def tree_as_rst_row(tree: PossiblyLayered[ReferenceTree]):
+    return [
+        tree.stems_per_ha,
+        tree.species,
+        tree.breast_height_diameter,
+        tree.height,
+        tree.breast_height_age,
+        tree.biological_age,
+        tree.saw_log_volume_reduction_factor,
+        tree.pruning_year,
+        tree.age_when_10cm_diameter_at_breast_height,
+        tree.origin,
+        tree.tree_number,
+        tree.stand_origin_relative_position[0],
+        tree.stand_origin_relative_position[1],
+        tree.stand_origin_relative_position[2],
+        tree.lowest_living_branch_height,
+        tree.management_category,
+        None,
+    ]
+
+
+def stratum_as_rsts_row(stratum: PossiblyLayered[TreeStratum]):
+    return [
+        stratum.tree_number,
+        stratum.species,
+        stratum.origin,
+        stratum.stems_per_ha,
+        stratum.mean_diameter,
+        stratum.mean_height,
+        stratum.breast_height_age,
+        stratum.biological_age,
+        stratum.basal_area,
+        stratum.sapling_stems_per_ha,
+        stratum.storey,
+        stratum.number_of_generated_trees
+    ]

commit 869086f10b062b458a4f5262dc6dd28c001acd27

cleanup: linter fixes (#124)
Fixed static analysis issues that were introduced by the previous PR
#122.

lukefi/metsi/app/simulator.py
@@ -18,6 +18,7 @@ from lukefi.metsi.sim.runners import Evaluator
 from lukefi.metsi.sim.runners import Runner
 from lukefi.metsi.sim.sim_configuration import SimConfiguration
 
+
 def run_stands(stands: StandList,
                config: SimConfiguration,
                runner: Runner[ForestStand],
@@ -29,7 +30,7 @@ def run_stands(stands: StandList,
     for stand in stands:
         overlaid_stand: PossiblyLayered[ForestStand]
         if stand.reference_trees_soa is None or stand.tree_strata_soa is None:
-            # If the state is not vectorized, wrap it as a LayeredObject so that new nodes in the EventTree don't have 
+            # If the state is not vectorized, wrap it as a LayeredObject so that new nodes in the EventTree don't have
             # to copy the entire state in memory and can just store the data that has actually changed instead.
             # This is not necessary for vectorized data since similar functionality is provided by the finalize method.
             overlaid_stand = LayeredObject[ForestStand](stand)

lukefi/metsi/data/formats/io_utils.py
@@ -5,7 +5,15 @@ from collections.abc import Callable
 from lukefi.metsi.app.app_types import ExportableContainer
 from lukefi.metsi.data.formats.util import parse_float
 from lukefi.metsi.data.layered_model import PossiblyLayered
-from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeStratum, stand_as_internal_csv_row, stand_as_rst_row, stratum_as_rsts_row, tree_as_internal_csv_row, tree_as_rst_row
+from lukefi.metsi.data.model import (
+    ForestStand,
+    ReferenceTree,
+    TreeStratum,
+    stand_as_internal_csv_row,
+    stand_as_rst_row,
+    stratum_as_rsts_row,
+    tree_as_internal_csv_row,
+    tree_as_rst_row)
 from lukefi.metsi.data.formats.rst_const import MSBInitialDataRecordConst as msb_meta
 from lukefi.metsi.domain.forestry_types import StandList
 

lukefi/metsi/data/vectorize.py
@@ -47,7 +47,6 @@ def vectorize(stands: StandList, **operation_params) -> StandList:
                 raise MetsiException(f"Unknown target type '{t}'")
             setattr(stand, f"{t}_soa", container_obj().vectorize(attr_dict))
             delattr(stand, t)
-        stand.vectorized = True
     return stands
 
 

pyproject.toml
@@ -5,7 +5,7 @@ build-backend = "setuptools.build_meta"
 [project]
 name = "lukefi.metsi"
 description = "Metsi forestry simulator."
-version = "3.2.1"
+version = "3.2.2"
 readme = "README.md"
 requires-python = ">=3.12"
 authors = [