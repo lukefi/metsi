358804d5a7a5097d050a7321b8081f3d45d02130
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-03 13:42:26 +0300

    convert planting to soa

--- lukefi/metsi/domain/forestry_operations/planting.py
+++ lukefi/metsi/domain/forestry_operations/planting.py
@@ -1,6 +1,8 @@
 from functools import cache
+from typing import Optional
+from lukefi.metsi.data.layered_model import PossiblyLayered
 from lukefi.metsi.sim.collected_data import CollectedData, OpTuple
-from lukefi.metsi.data.model import ForestStand, create_layered_tree
+from lukefi.metsi.data.model import ForestStand
 from lukefi.metsi.data.enums.internal import TreeSpecies
 from lukefi.metsi.domain.utils.enums import SiteTypeKey, SoilPreparationKey, RegenerationKey
 from lukefi.metsi.domain.utils.conversion import site_type_to_key
@@ -33,12 +35,11 @@ DEFAULT_INSTRUCTIONS = {
 
 
 @cache
-def create_planting_instructions_table(file_path: str) -> list:
+def _create_planting_instructions_table(file_path: str) -> list:
     contents = None
     with open(file_path, "r", encoding="utf-8") as f:
         contents = f.read()
-    table = contents.split('\n')
-    table = [row.split() for row in table]
+    table = [row.split() for row in contents.split('\n')]
 
     if len(table) != 4 or len(table[0]) != 3:
         raise MetsiException("Planting instructions file has unexpected structure. Expected 4 rows and 5 columns, "
@@ -46,10 +47,8 @@ def create_planting_instructions_table(file_path: str) -> list:
     return table
 
 
-def get_planting_instructions_from_parameter_file_contents(
-    file_path: str,
-) -> dict:
-    instructions_table = create_planting_instructions_table(file_path)
+def _get_planting_instructions_from_parameter_file_contents(file_path: str) -> dict:
+    instructions_table = _create_planting_instructions_table(file_path)
     instructions = {
         SiteTypeKey.OMT: {
             'species': int(instructions_table[0][0]),
@@ -75,45 +74,44 @@ def get_planting_instructions_from_parameter_file_contents(
     return instructions
 
 
-def get_planting_instructions(site_type_category: int, file_path_instructions: str = None) -> dict:
+def _get_planting_instructions(site_type_category: int, file_path_instructions: Optional[str] = None) -> dict:
     site_type_key = site_type_to_key(site_type_category)
     if file_path_instructions is not None:
-        instructions = get_planting_instructions_from_parameter_file_contents(file_path_instructions)
+        instructions = _get_planting_instructions_from_parameter_file_contents(file_path_instructions)
         regen = instructions[site_type_key]
     else:
         regen = DEFAULT_INSTRUCTIONS[site_type_key]
     return regen
 
 
-def plant(
-    stand: ForestStand,
-    collected_data: CollectedData,
-    tag: str,
-    regen_species: TreeSpecies,
-    rt_count: int,
-    rt_stems: int,
-    soil_preparation: SoilPreparationKey
-) -> OpTuple[ForestStand]:
+def _plant(stand: PossiblyLayered[ForestStand],
+           collected_data: CollectedData,
+           tag: str,
+           regen_species: TreeSpecies,
+           rt_count: int,
+           rt_stems: int,
+           soil_preparation: SoilPreparationKey
+           ) -> OpTuple[ForestStand]:
 
     regeneration_description = {
         'regeneration': RegenerationKey.PLANTED,
         'soil preparation': soil_preparation,
         'species': regen_species,
-        'stems_per_ha': rt_count*rt_stems
+        'stems_per_ha': rt_count * rt_stems
     }
 
-    stand.reference_trees = [
-        create_layered_tree(
-            identifier=stand.identifier + f"-{i}-tree",
-            stems_per_ha=rt_stems/rt_count,
-            species=regen_species,
-            breast_height_diameter=0,
-            breast_height_age=0,
-            biological_age=1,
-            height=0.3,
-            sapling=True)
-        for i in range(rt_count)
-    ]
+    stand.reference_trees.create([
+        {
+            "identifier": stand.identifier + f"-{i}-tree",
+            "stems_per_ha": rt_stems / rt_count,
+            "species": regen_species,
+            "breast_height_diameter": 0,
+            "breast_height_age": 0,
+            "biological_age": 1,
+            "height": 0.3,
+            "sapling": True
+        } for i in range(rt_count)
+    ])
 
     collected_data.store(tag, regeneration_description)
     collected_data.extend_list_result(
@@ -136,13 +134,15 @@ def planting(payload: OpTuple[ForestStand], /, **operation_parameters) -> OpTupl
         return payload
 
     instructions_path = operation_parameters.get('planting_instructions', None)
-    regen = get_planting_instructions(stand.site_type_category, instructions_path)
-    stand, output_planting = plant(
-        stand,
-        collected_data,
-        "regeneration",
-        TreeSpecies(regen['species']),
-        tree_count,
-        regen['stems/ha'],
-        regen['soil preparation'])
+    if stand.site_type_category is None:
+        raise MetsiException(f"No site type category defined for stand {stand.stand_id}")
+
+    regen = _get_planting_instructions(int(stand.site_type_category), instructions_path)
+    stand, output_planting = _plant(stand,
+                                    collected_data,
+                                    "regeneration",
+                                    TreeSpecies(regen['species']),
+                                    tree_count,
+                                    regen['stems/ha'],
+                                    regen['soil preparation'])
     return (stand, output_planting)
