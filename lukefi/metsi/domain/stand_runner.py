2867fab924f1626dd8857696c07e1febd99063f3
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-03 14:12:04 +0300

    remove vectorization checks from stand_runner

--- lukefi/metsi/domain/stand_runner.py
+++ lukefi/metsi/domain/stand_runner.py
@@ -1,6 +1,6 @@
 from lukefi.metsi.app.console_logging import print_logline
-from lukefi.metsi.data.layered_model import LayeredObject, PossiblyLayered
-from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeStratum
+from lukefi.metsi.data.layered_model import LayeredObject
+from lukefi.metsi.data.model import ForestStand
 from lukefi.metsi.domain.forestry_types import ForestOpPayload, StandList
 from lukefi.metsi.sim.collected_data import CollectedData
 from lukefi.metsi.sim.runners import Evaluator, TreeRunner
@@ -16,17 +16,7 @@ def run_stands(stands: StandList,
 
     retval: dict[str, list[ForestOpPayload]] = {}
     for stand in stands:
-        overlaid_stand: PossiblyLayered[ForestStand]
-        if stand.reference_trees_soa is None or stand.tree_strata_soa is None:
-            # If the state is not vectorized, wrap it as a LayeredObject so that new nodes in the EventTree don't have
-            # to copy the entire state in memory and can just store the data that has actually changed instead.
-            # This is not necessary for vectorized data since similar functionality is provided by the finalize method.
-            overlaid_stand = LayeredObject[ForestStand](stand)
-            overlaid_stand.reference_trees = [LayeredObject[ReferenceTree]
-                                              (tree) for tree in overlaid_stand.reference_trees]
-            overlaid_stand.tree_strata = [LayeredObject[TreeStratum](stratum) for stratum in overlaid_stand.tree_strata]
-        else:
-            overlaid_stand = stand
+        overlaid_stand = LayeredObject[ForestStand](stand)
 
         payload = ForestOpPayload(
             computational_unit=overlaid_stand,
