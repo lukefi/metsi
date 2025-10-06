0fff1695b1a1b3d472aa684033c57b97a409cc00
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-06 12:56:54 +0300

    cleanup: remove data/conversion/util.py

--- lukefi/metsi/data/conversion/util.py
+++
@@ -1,8 +0,0 @@
-from collections.abc import Callable
-
-
-def apply_mappers(target, *mappers: Callable):
-    """apply a list of mapper functions to a target object"""
-    for mapper in mappers:
-        target = mapper(target)
-    return target
