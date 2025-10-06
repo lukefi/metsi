cea72cfdf5093d93eb4962e15c229ba553dcb7d3
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-06 12:45:27 +0300

    cleanup: remove export modes rm_schedules_events_timber and rm_schedules_events_trees

--- lukefi/metsi/app/export.py
+++ lukefi/metsi/app/export.py
@@ -1,5 +1,4 @@
 import copy
-from pathlib import Path
 from functools import partial
 from typing import Any, Callable, Optional
 
@@ -7,7 +6,6 @@ from lukefi.metsi.app.app_io import MetsiConfiguration
 from lukefi.metsi.app.app_types import ExportableContainer
 from lukefi.metsi.app.export_handlers.j import j_out, parse_j_config
 from lukefi.metsi.app.console_logging import print_logline
-from lukefi.metsi.app.export_handlers.rm_timber import rm_schedules_events_timber, rm_schedules_events_trees
 from lukefi.metsi.app.file_io import write_stands_to_file, determine_file_path
 from lukefi.metsi.domain.forestry_types import SimResults, StandList
 from lukefi.metsi.sim.operations import simple_processable_chain
@@ -21,14 +19,6 @@ def export_files(config: MetsiConfiguration, decl: list[dict], data: SimResults)
         if export_module == "J":
             j_config = parse_j_config(config, export_module_declaration)
             output_handlers.append((export_module, partial(j_out, data, **j_config)))
-        elif export_module == "rm_schedules_events_timber":
-            filename1 = export_module_declaration.get("filename", "rm_schedule_timber_year_sums.txt")
-            target_path1 = Path(config.target_directory, filename1)
-            output_handlers.append((export_module, partial(rm_schedules_events_timber, target_path1, data)))
-        elif export_module == "rm_schedules_events_trees":
-            filename2 = export_module_declaration.get("filename", "rm_schedule_trees.txt")
-            target_path2 = Path(config.target_directory, filename2)
-            output_handlers.append((export_module, partial(rm_schedules_events_trees, target_path2, data)))
         else:
             print_logline(f"Unknown output format for export: '{export_module}'")
     for export_module, handler in output_handlers:
