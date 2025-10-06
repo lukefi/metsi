ac6391573d4a17162b233afe348ae805b9844e9d
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-06 12:49:57 +0300

    cleanup: remove unused pre_util functions

--- lukefi/metsi/forestry/preprocessing/pre_util.py
+++ lukefi/metsi/forestry/preprocessing/pre_util.py
@@ -1,21 +1,11 @@
 """ Module contains basic, domain and state spesific utility functions used in preprocessing operations"""
-from typing import Optional, Any
 from lukefi.metsi.data.model import TreeStratum
-
-
-# ---- basic utils ----
-
-def get_or_default(maybe: Optional[Any], default: Any = None) -> Any:
-    return default if maybe is None else maybe
 
 
 # ---- state utils ----
 
-def stratum_needs_diameter(stratum: TreeStratum) -> bool:
-    return not stratum.has_diameter() and stratum.has_height_over_130_cm()
-
 
 def supplement_mean_diameter(stratum: TreeStratum) -> TreeStratum:
     diameter_factor = 1.2
-    stratum.mean_diameter = stratum.mean_height * diameter_factor
+    stratum.mean_diameter = (stratum.mean_height * diameter_factor) if stratum.mean_height is not None else None
     return stratum
